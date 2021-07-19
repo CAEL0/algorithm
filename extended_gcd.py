b97fbc6e593790bea17e8fe49dc7194c29ccac3e
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-07-19 20:57:54 +0900

    Create extended_gcd.py

---
+++ extended_gcd.py
@@ -0,0 +1,13 @@
+"""
+In arithmetic and computer programming, the extended Euclidean algorithm is an extension to the Euclidean
+algorithm, and computes, in addition to the greatest common divisor (gcd) of integers a and b, also the coefficients
+of Bézout's identity, which are integers x and y such that ax + by = gcd(a, b).
+"""
+
+def extended_gcd(a, b):
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while b != 0:
+        n, a, b = a // b, b, a % b
+        x0, x1 = x1, x0 - n * x1
+        y0, y1 = y1, y0 - n * y1
+    return x0, y0
