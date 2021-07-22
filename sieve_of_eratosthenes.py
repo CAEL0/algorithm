a674705aa2f9638b47c4d9f0a1591ef932d46314
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-07-22 12:45:41 +0900

    Update sieve_of_eratosthenes.py

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -11,7 +11,7 @@ sieve[1] = 0
 for i in range(2, n + 1):
     if sieve[i]:
         check[i] = i
-        for j in range(2, (n // i + 1)):
+        for j in range(2, n // i + 1):
             if sieve[j * i]:
                 check[j * i] = i
             sieve[j * i] = 0
