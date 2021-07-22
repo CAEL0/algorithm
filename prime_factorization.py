82546c83519cfec7b80e1863d27d978c7cf49fe4
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-07-22 10:01:47 +0900

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -2,14 +2,14 @@ from collections import defaultdict
 
 n = int(input())
 m = int(n ** 0.5)
-prime_list = [1] * (m + 1)
-prime_list[0] = 0
-prime_list[1] = 0
+sieve = [1] * (m + 1)
+sieve[0] = 0
+sieve[1] = 0
 
 for i in range(2, int((m + 1) ** 0.5) + 1):
-    if prime_list[i] == 1:
+    if sieve[i]:
         for j in range(2, (m // i + 1)):
-            prime_list[j * i] = 0
+            sieve[j * i] = 0
 
 
 def factorization(k):
@@ -18,7 +18,7 @@ def factorization(k):
     m = k ** 0.5
     while (k > 1) and (idx < m):
         idx += 1
-        if prime_list[idx] == 0:
+        if sieve[idx] == 0:
             continue
 
         while k % idx == 0:
