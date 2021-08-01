862c0e94c89b21d551bf1608b419a8afc2d28764
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-08-01 22:56:52 +0900

    Update 2sat.py

--- 2sat.py
+++ 2sat.py
@@ -67,4 +67,63 @@ else:
     print(1)
     print(*res)
 
----
+----------------------------------------------------------------------------------------------------
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(2 * n + 1)]
+reverse = [[] for _ in range(2 * n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[-a].append(b)
+    graph[-b].append(a)
+    reverse[b].append(-a)
+    reverse[a].append(-b)
+
+stack = []
+visit = [0] * (2 * n + 1)
+for i in range(1, 2 * n + 1):
+    if not visit[i]:
+        visit[i] = 1
+        queue = [i]
+        while queue:
+            cur = queue[-1]
+            for nxt in graph[cur]:
+                if not visit[nxt]:
+                    visit[nxt] = 1
+                    queue.append(nxt)
+                    break
+            else:
+                stack.append(queue.pop())
+
+scc_num = 0
+scc_idx = [0] * (2 * n + 1)
+finish = [0] * (2 * n + 1)
+while stack:
+    node = stack.pop()
+    if not finish[node]:
+        finish[node] = 1
+        queue = [node]
+        while queue:
+            cur = queue[-1]
+            for nxt in reverse[cur]:
+                if not finish[nxt]:
+                    finish[nxt] = 1
+                    queue.append(nxt)
+                    break
+            else:
+                scc_idx[queue.pop()] = scc_num
+
+        scc_num += 1
+
+res = [0] * n
+for i in range(1, n + 1):
+    if scc_idx[i] == scc_idx[-i]:
+        print(0)
+        break
+    if scc_idx[i] > scc_idx[-i]:
+        res[i - 1] = 1
+else:
+    print(1)
+    print(*res)
