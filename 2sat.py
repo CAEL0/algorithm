3393500ed98111bc894b66f9f886f4b6a1bb6f17
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-08-01 22:55:10 +0900

    Create 2sat.py

---
+++ 2sat.py
@@ -0,0 +1,70 @@
+"""
+In computer science, 2-satisfiability, 2-SAT or just 2SAT is a computational problem of assigning values to
+variables, each of which has two possible values, in order to satisfy a system of constraints on pairs of variables.
+It is a special case of the general Boolean satisfiability problem, which can involve constraints on more than two
+variables, and of constraint satisfaction problems, which can allow more than two choices for the value of each
+variable. But in contrast to those more general problems, which are NP-complete, 2-satisfiability can be solved in
+polynomial time.
+"""
+
+import sys
+sys.setrecursionlimit(10 ** 5)
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(2 * n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[-a].append(b)
+    graph[-b].append(a)
+
+stack = []
+scc_num = 1
+scc_idx = [0] * (2 * n + 1)
+finish = [0] * (2 * n + 1)
+visit = [0] * (2 * n + 1)
+idx = 1
+
+
+def dfs(cur):
+    global idx, scc_num
+    visit[cur] = idx
+    low = idx
+    idx += 1
+    stack.append(cur)
+
+    for nxt in graph[cur]:
+        if not visit[nxt]:
+            low = min(low, dfs(nxt))
+        elif not finish[nxt]:
+            low = min(low, visit[nxt])
+
+    if low == visit[cur]:
+        while stack:
+            top = stack.pop()
+            finish[top] = 1
+            scc_idx[top] = scc_num
+            if cur == top:
+                break
+
+        scc_num += 1
+
+    return low
+
+
+for i in range(1, 2 * n + 1):
+    if not visit[i]:
+        dfs(i)
+
+res = [0] * n
+for i in range(1, n + 1):
+    if scc_idx[i] == scc_idx[-i]:
+        print(0)
+        break
+    if scc_idx[i] < scc_idx[-i]:
+        res[i - 1] = 1
+else:
+    print(1)
+    print(*res)
+
+---
