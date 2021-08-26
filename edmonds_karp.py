b7d80faa73975274285662d0a3c5fd226fee6092
Jongmin Kim <kjongmin26@gmail.com>
2021-08-26 10:13:03 +0900

    Update edmonds_karp.py

--- edmonds_karp.py
+++ edmonds_karp.py
@@ -49,3 +49,63 @@ while True:
             break
 
 print(ans)
+
+#--------------------------------------------------------------------------------
+
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    start, end, capacity = map(int, sys.stdin.readline().split())
+    graph[start].append([end, capacity])
+
+ans = 0
+while True:
+    prev = [-1] * (n + 1)
+    queue = deque([(1, float('inf'))])
+    while queue:
+        cur, res = queue.popleft()
+        
+        for nxt, capacity in graph[cur]:
+            if nxt == n:
+                res = min(res, capacity)
+                prev[nxt] = cur
+                break
+               
+            if prev[nxt] == -1:
+                prev[nxt] = cur
+                queue.append((nxt, min(res, graph[cur][nxt])))
+
+        if prev[-1] != -1:
+            break
+    
+    if prev[-1] == -1:
+        break
+
+    ans += res
+    nxt = n
+    while True:
+        cur = prev[nxt]
+        
+        for i in range(len(graph[cur])):
+            if graph[cur][i][0] == nxt:
+                graph[cur][i][1] -= res
+                if graph[cur][i][1] == 0:
+                    graph[cur].remove([nxt, 0])
+                break
+        
+        for i in range(len(graph[nxt])):
+            if graph[nxt][i][0] == cur:
+                graph[nxt][i][1] += res
+                break
+        else:
+            graph[nxt].append([cur, res])
+
+        nxt = cur
+        if nxt == 1:
+            break
+
+print(ans)
