b3f1f9879531587df694a73d7a7977670269f91a
Jongmin Kim <kjongmin26@gmail.com>
2021-08-26 10:20:30 +0900

    Create bipartite_matching.py

---
+++ bipartite_matching.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def dfs(cur):
+    visit[cur] = 1
+    for nxt in graph[cur]:
+        if not b[nxt] or (not visit[b[nxt]] and dfs(b[nxt])):
+            b[nxt] = cur
+            return 1
+    return 0
+
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    data = list(map(int, sys.stdin.readline().split()))
+    for j in range(1, data[0] + 1):
+        graph[i].append(data[j])
+
+b = [0] * (m + 1)
+ans = 0
+
+for i in range(1, n + 1):
+    visit = [0] * (n + 1)
+    ans += dfs(i)
+
+print(ans)
