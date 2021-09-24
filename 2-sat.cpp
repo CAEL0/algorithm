345262de3bed36e93eeae10ed77d7820b4ef340d
Jongmin Kim <kjongmin26@gmail.com>
2021-09-24 18:44:13 +0900

    Update 2-sat.cpp

--- 2-sat.cpp
+++ 2-sat.cpp
@@ -13,7 +13,7 @@ polynomial time.
 #include <algorithm>
 
 using namespace std;
-int MAX = 987654321;
+int MAX = 987654;
 int idx, scc_num;
 stack<int> S;
 vector<int> visit(MAX), scc_idx(MAX), finish(MAX);
@@ -59,8 +59,24 @@ int main() {
     while (m--) {
         int x, y;
         cin >> x >> y;
-        group[2 * x].push_back(2 * y - 1);
-        group[2 * y].push_back(2 * x - 1);
+        if (x < 0) {
+            if (y < 0) {
+                graph[(-2) * x - 1].push_back((-2) * y);
+                graph[(-2) * y - 1].push_back((-2) * x);
+            }
+            else {
+                graph[(-2) * x - 1].push_back(2 * y - 1);
+                graph[2 * y].push_back((-2) * x);
+            }
+        }
+        else if (y < 0) {
+            graph[2 * x].push_back((-2) * y);
+            graph[(-2) * y - 1].push_back(2 * x - 1);
+        }
+        else {
+            graph[2 * x].push_back(2 * y - 1);
+            graph[2 * y].push_back(2 * x - 1);
+        }
     }
 
 	idx = 1;
@@ -72,18 +88,16 @@ int main() {
 		}
 	}
     
-    vector<int> res(n + 1);
 	bool flag = true;
-	for (int i = 1; i < 2 * n; i += 2) {
-		if (scc_idx[i] == scc_idx[i + 1]) {
+	for (int i = 0; i < n; i++) {
+		if (scc_idx[2 * i + 1] == scc_idx[2 * i + 2]) {
 			cout << '0';
 			flag = false;
 			break;
-        if (scc_idx[i] < scc_idx[i + 1]) res[i] = 1;
 		}
 	}
 	if (flag) {
         cout << '1' << '\n';
-        for (int i = 1; i < n + 1; i++) cout << res[i] << ' ';
+        for (int i = 0; i < n; i++) cout << (scc_idx[2 * i + 1] < scc_idx[2 * i + 2]) << ' ';
     }
 }
