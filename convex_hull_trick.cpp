3e44e13f2b1c8d3eac3a396b5b9f18b5abfa13c6
Jongmin Kim <kjongmin26@gmail.com>
2021-10-09 10:32:18 +0900

    Update convex_hull_trick.cpp

--- convex_hull_trick.cpp
+++ convex_hull_trick.cpp
@@ -1,57 +1,58 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#define fi first
+#define se second
+#define sz size()
+#define bk back()
 
 using namespace std;
-typedef pair<long long, long long> pll;
+typedef long long ll;
+typedef pair<ll, ll> pll;
 
-vector<pll> stk;
+vector<pll> STK;
 
 double cross(int j, int k) {
-    long long x1, y1, x2, y2;
-    tie(x1, y1) = stk[j];
-    tie(x2, y2) = stk[k];
-    return (y2 - y1) / (x1 - x2);
+    ll x1, y1, x2, y2;
+    tie(x1, y1) = STK[j];
+    tie(x2, y2) = STK[k];
+    return (double)(y2 - y1) / (x1 - x2);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    int tmp, a[n], b[n];
-    for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        a[i] = tmp;
+    int A[N], B[N];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-    for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        b[i] = tmp;
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
     }
-
-    stk.push_back(pll(b[0], 0));
+    STK.push_back(pll(B[0], 0));
     
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < N; i++) {
         int left = 0;
-        int right = stk.size() - 1;
+        int right = STK.sz - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (cross(mid, mid + 1) <= a[i]) {
+            if (cross(mid, mid + 1) <= A[i]) {
                 left = mid + 1;
             }
             else {
                 right = mid;
             }
         }
-        stk.push_back(pll(b[i], stk[left].first * a[i] + stk[left].second));
-        while (stk.size() > 2 && cross(stk.size() - 2, stk.size() - 1) <= cross(stk.size() - 3, stk.size() - 2)) {
-            long long x, y;
-            tie(x, y) = stk.back();
-            stk.pop_back();
-            stk.pop_back();
-            stk.push_back(pll(x, y));
+        STK.push_back(pll(B[i], STK[left].fi * A[i] + STK[left].se));
+        while (STK.sz > 2 && cross(STK.sz - 2, STK.sz - 1) <= cross(STK.sz - 3, STK.sz - 2)) {
+            pll tmp = STK.back();
+            STK.pop_back();
+            STK.pop_back();
+            STK.push_back(tmp);
         }
     }
-    cout << stk.back().second;
+    cout << STK.bk.se;
 }
