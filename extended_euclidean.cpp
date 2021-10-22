61bca0b2066dd4f9668cbda1a2035ddab475e614
Jongmin Kim <kjongmin26@gmail.com>
2021-10-22 19:11:57 +0900

    Create extended_euclidean.cpp

---
+++ extended_euclidean.cpp
@@ -0,0 +1,19 @@
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+pll extended_gcd(ll x, ll y) {
+    ll x0 = 1, y1 = 1, x1 = 0, y0 = 0, n, tmp;
+    while (y) {
+        n = x / y;
+        tmp = x;
+        x = y;
+        y = tmp % y;
+        tmp = x0;
+        x0 = x1;
+        x1 = tmp - n * x1;
+        tmp = y0;
+        y0 = y1;
+        y1 = tmp - n * y1;
+    }
+    return pll(x0, y0);
+}
