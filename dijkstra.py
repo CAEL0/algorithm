7a43bc42e2efe6c2e746a7fdf123427287c82ad7
Jongmin Kim <kjongmin26@gmail.com>
2021-10-31 13:38:00 +0900

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -16,17 +16,13 @@ v, e = map(int, sys.stdin.readline().split())
 def dijkstra(graph, start):
     res = {node: float('inf') for node in range(1, v + 1)}
     res[start] = 0
-    queue = [[0, start]]
-
+    queue = [(0, start)]
     while queue:
         old_weight, old_node = heappop(queue)
-        if res[old_node] < old_weight:
-            continue
-
-        for new_node, new_weight in graph[old_node].items():
-            total_weight = old_weight + new_weight
-            if total_weight < res[new_node]:
-                res[new_node] = total_weight
-                heappush(queue, [total_weight, new_node])
-
+        if res[old_node] >= old_weight:
+            for new_node, new_weight in graph[old_node].items():
+                total_weight = old_weight + new_weight
+                if total_weight < res[new_node]:
+                    res[new_node] = total_weight
+                    heappush(queue, (total_weight, new_node))
     return res
