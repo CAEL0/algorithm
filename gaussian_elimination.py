1fb2db6b00931a4e24ac8c6332dd6ccbf51ff8a3
Jongmin Kim <kjongmin26@gmail.com>
2021-11-08 10:59:34 +0900

    Update gaussian_elimination.py

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,5 +1,7 @@
 """
-
+In mathematics, Gaussian elimination, also known as row reduction, is an algorithm for solving systems of linear equations.
+It consists of a sequence of operations performed on the corresponding matrix of coefficients.
+This method can also be used to compute the rank of a matrix, the determinant of a square matrix, and the inverse of an invertible matrix.
 """
 
 import sys
