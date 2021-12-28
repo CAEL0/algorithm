23c5b1b28f2f82eb057c3328b95e25f5932f833e
Jongmin Kim <kjongmin26@gmail.com>
2021-12-28 17:35:34 +0900

    Update tarjan.py

--- tarjan.py
+++ tarjan.py
@@ -10,7 +10,6 @@ sys.setrecursionlimit(10 ** 5)
 
 n, m = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n + 1)]
-
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
