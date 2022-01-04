d8fdcbd3459b425852fbe1e3a663b8259863086f
Jongmin Kim <kjongmin26@gmail.com>
2022-01-04 22:50:28 +0900

    Update suffix_array_and_lcp_array.py

--- suffix_array_and_lcp_array.py
+++ suffix_array_and_lcp_array.py
@@ -47,3 +47,71 @@ for i in range(n):
     k -= (k > 0)
 
 print(*lcp)
+
+#--------------------------------------------------------------------------------
+
+import sys
+
+s = sys.stdin.readline().strip()
+n = len(s)
+sa = [i for i in range(n)]
+pos = [ord(s[i]) for i in range(n)] + [0]
+d = 1
+while d <= n:
+    cnt = [0] * max(n, 200)
+    cnt[0] = d
+    for i in range(d, n):
+        cnt[pos[i]] += 1
+    
+    for i in range(1, len(cnt)):
+        cnt[i] += cnt[i - 1]
+    
+    idx = [0] * n
+    for i in range(n - 1, -1, -1):
+        cnt[pos[min(i + d, n)]] -= 1
+        idx[cnt[pos[min(i + d, n)]]] = i
+    
+    cnt = [0] * max(n, 200)
+    for i in range(n):
+        cnt[pos[i]] += 1
+    
+    for i in range(1, len(cnt)):
+        cnt[i] += cnt[i - 1]
+    
+    for i in range(n - 1, -1, -1):
+        cnt[pos[idx[i]]] -= 1
+        sa[cnt[pos[idx[i]]]] = idx[i]
+    
+    tmp = [0] * n
+    for i in range(n - 1):
+        x, y = sa[i], sa[i + 1]
+        tmp[i + 1] = tmp[i] + ((pos[x], pos[min(x + d, n)]) < (pos[y], pos[min(y + d, n)]))
+    
+    for i in range(n):
+        pos[sa[i]] = tmp[i]
+    
+    if tmp[-1] == n - 1:
+        break
+    
+    d *= 2
+
+for i in range(n):
+    print(sa[i] + 1, end=' ')
+print()
+
+lcp = [0] * n
+lcp[0] = 'x'
+k = 0
+for i in range(n):
+    if pos[i] == 0:
+        k -= (k > 0)
+        continue
+    
+    j = sa[pos[i] - 1]
+    while (max(i, j) + k < n) and (s[i + k] == s[j + k]):
+        k += 1
+    
+    lcp[pos[i]] = k
+    k -= (k > 0)
+
+print(*lcp)
