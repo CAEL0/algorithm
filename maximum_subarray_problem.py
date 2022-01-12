d198914596f786a8c55f08337fbe39812fe56848
Jongmin Kim <kjongmin26@gmail.com>
2022-01-12 17:35:13 +0900

    Update maximum_subarray_problem.py

--- maximum_subarray_problem.py
+++ maximum_subarray_problem.py
@@ -1,3 +1,7 @@
+"""
+In computer science, the maximum sum subarray problem is the task of finding a contiguous subarray with the largest sum, within a given one-dimensional array A[1...n] of numbers.
+"""
+
 import sys
 from math import log2, ceil
 
