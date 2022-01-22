e1948836e465b338228e18b40413713731deb533
Jongmin Kim <kjongmin26@gmail.com>
2022-01-22 17:18:08 +0900

    Create disjoint_set.cpp

---
+++ disjoint_set.cpp
@@ -0,0 +1,37 @@
+// BOJ 1717
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int parent[1000000];
+
+int find(int z) {
+    if (z != parent[z])
+        parent[z] = find(parent[z]);
+    return parent[z];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m, o, x, y;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+        parent[i] = i;
+
+    while (m--) {
+        cin >> o >> x >> y;
+        if (o) {
+            if (find(x) == find(y))
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+        else
+            parent[find(x)] = find(y);
+    }
+}
