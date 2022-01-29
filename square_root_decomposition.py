efa61adb0010b8400ad0bdaa4ed8319f9cc83079
Jongmin Kim <kjongmin26@gmail.com>
2022-01-29 18:53:44 +0900

    Update square_root_decomposition.py

--- square_root_decomposition.py
+++ square_root_decomposition.py
@@ -28,3 +28,42 @@ for _ in range(q):
         ans[1] = max(ans[1], arr[i])
     
     print(*ans)
+
+#--------------------------------------------------------------------------------
+
+import sys
+
+n, m, o = map(int, sys.stdin.readline().split())
+k = int(n ** 0.5)
+arr = [int(sys.stdin.readline()) for _ in range(n)]
+group = [0] * (k + 2)
+
+for i in range(n):
+    idx = i // k
+    group[idx] += arr[i]
+
+for _ in range(m + o):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == 1:
+        b -= 1
+        idx = b // k
+        gap = c - arr[b]
+        arr[b] = c
+        group[idx] += gap
+    else:
+        b -= 1
+        c -= 1
+        idx1 = b // k
+        idx2 = c // k
+        ans = 0
+        for i in range(idx1 + 1, idx2):
+            ans += group[i]
+
+        for i in range(b, min(k * (idx1 + 1), c + 1)):
+            ans += arr[i]
+
+        if idx1 != idx2:
+            for i in range(max(b, k * idx2), c + 1):
+                ans += arr[i]
+
+        print(ans)
