f193d242e18d0276c130be076220a12a6b722a07
Jongmin Kim <kjongmin26@gmail.com>
2022-01-29 18:41:45 +0900

    Create square_root_decomposition.py

---
+++ square_root_decomposition.py
@@ -0,0 +1,30 @@
+import sys
+
+n, q = map(int, sys.stdin.readline().split())
+k = int(n ** 0.5)
+arr = [int(sys.stdin.readline()) for _ in range(n)]
+group = [[float('inf'), -float('inf')] for _ in range(k + 2)]
+
+for i in range(n):
+    idx = i // k
+    group[idx][0] = min(group[idx][0], arr[i])
+    group[idx][1] = max(group[idx][1], arr[i])
+
+for _ in range(q):
+    a, b = map(lambda x: int(x) - 1, sys.stdin.readline().split())
+    idx1 = a // k
+    idx2 = b // k
+    ans = [float('inf'), -float('inf')]
+    for i in range(idx1 + 1, idx2):
+        ans[0] = min(ans[0], group[i][0])
+        ans[1] = max(ans[1], group[i][1])
+    
+    for i in range(a, min(k * (idx1 + 1), b + 1)):
+        ans[0] = min(ans[0], arr[i])
+        ans[1] = max(ans[1], arr[i])
+    
+    for i in range(max(a, k * idx2), b + 1):
+        ans[0] = min(ans[0], arr[i])
+        ans[1] = max(ans[1], arr[i])
+    
+    print(*ans)
