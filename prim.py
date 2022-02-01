3df200adf1b536200bd0f61f6c92f4010b532686
Jongmin Kim <kjongmin26@gmail.com>
2022-02-01 22:03:22 +0900

    Update prim.py

--- prim.py
+++ prim.py
@@ -1,9 +1,8 @@
 """
-In computer science, Prim's (also known as Jarník's) algorithm is a greedy algorithm that finds a minimum spanning
-tree for a weighted undirected graph. This means it finds a subset of the edges that forms a tree that includes every
-vertex, where the total weight of all the edges in the tree is minimized. The algorithm operates by building this
-tree one vertex at a time, from an arbitrary starting vertex, at each step adding the cheapest possible connection
-from the tree to another vertex.
+In computer science, Prim's (also known as Jarník's) algorithm is a greedy algorithm that finds a minimum spanning tree for a weighted undirected graph.
+This means it finds a subset of the edges that forms a tree that includes every vertex, where the total weight of all the edges in the tree is minimized.
+The algorithm operates by building this tree one vertex at a time, from an arbitrary starting vertex,
+at each step adding the cheapest possible connection from the tree to another vertex.
 """
 
 import sys
