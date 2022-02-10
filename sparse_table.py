61484a0d1711ec284dea79b17ea657347c002c49
Jongmin Kim <kjongmin26@gmail.com>
2022-02-10 14:01:02 +0900

    Update sparse_table.py

--- sparse_table.py
+++ sparse_table.py
@@ -3,12 +3,12 @@ import sys
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 sparse = [[0] * (n + 1) for _ in range(20)]
-for j in range(1, n + 1):
-    sparse[0][j] = arr[j - 1]
+for j in range(n):
+    sparse[0][j + 1] = arr[j]
 
-for i in range(1, 20):
+for i in range(19):
     for j in range(1, n + 1):
-        sparse[i][j] = sparse[i - 1][sparse[i - 1][j]]
+        sparse[i + 1][j] = sparse[i][sparse[i][j]]
 
 for _ in range(int(sys.stdin.readline())):
     n, x = map(int, sys.stdin.readline().split())
