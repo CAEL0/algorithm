318bb9193c4013738e0f3d05b285eeb6e9d0bf3a
Jongmin Kim <kjongmin26@gmail.com>
2022-02-11 18:31:57 +0900

    Update bellman_ford.py

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,10 +1,8 @@
 """
-The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the
-other vertices in a weighted digraph.[1] It is slower than Dijkstra's algorithm for the same problem,
-but more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers. The
-algorithm was first proposed by Alfonso Shimbel (1955), but is instead named after Richard Bellman and Lester Ford
-Jr., who published it in 1958 and 1956, respectively.[2] Edward F. Moore also published a variation of the algorithm
-in 1959, and for this reason it is also sometimes called the Bellman–Ford–Moore algorithm.
+The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph.
+It is slower than Dijkstra's algorithm for the same problem, but more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers.
+The algorithm was first proposed by Alfonso Shimbel (1955), but is instead named after Richard Bellman and Lester Ford Jr., who published it in 1958 and 1956, respectively.
+Edward F. Moore also published a variation of the algorithm in 1959, and for this reason it is also sometimes called the Bellman–Ford–Moore algorithm.
 """
 
 import sys
