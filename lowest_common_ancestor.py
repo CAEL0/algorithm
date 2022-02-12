0983940b99eb8586ca2900708e8a10e3515d09f4
Jongmin Kim <kjongmin26@gmail.com>
2022-02-12 16:06:30 +0900

    Create lowest_common_ancestor.py

---
+++ lowest_common_ancestor.py
@@ -0,0 +1,48 @@
+import sys
+
+n = int(sys.stdin.readline())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+sparse = [[1] * (n + 1) for _ in range(20)]
+queue = [(1, 2)]
+depth = [1] * (n + 1)
+visit = [0] * (n + 1)
+visit[1] = 1
+while queue:
+    cur, d = queue.pop()
+    for nxt in graph[cur]:
+        if not visit[nxt]:
+            depth[nxt] = d
+            visit[nxt] = 1
+            sparse[0][nxt] = cur
+            queue.append((nxt, d + 1))
+
+for i in range(19):
+    for j in range(1, n + 1):
+        sparse[i + 1][j] = sparse[i][sparse[i][j]]
+
+for _ in range(int(sys.stdin.readline())):
+    a, b = map(int, sys.stdin.readline().split())
+    if depth[a] > depth[b]:
+        a, b = b, a
+    d = depth[b] - depth[a]
+    for i in range(20):
+        if d & (1 << i):
+            b = sparse[i][b]
+    
+    if a == b:
+        print(a)
+        continue
+    
+    while True:
+        for i in range(19, -1, -1):
+            if sparse[i][a] != sparse[i][b]:
+                a = sparse[i][a]
+                b = sparse[i][b]
+        if sparse[0][a] == sparse[0][b]:
+            print(sparse[0][a])
+            break
