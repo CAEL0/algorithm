321d1383414417f13fa0ed7f5f44fab3803da2e5
Jongmin Kim <kjongmin26@gmail.com>
2022-02-12 16:07:26 +0900

    Update lowest_common_ancestor.py

--- lowest_common_ancestor.py
+++ lowest_common_ancestor.py
@@ -1,3 +1,9 @@
+"""
+In graph theory and computer science, the lowest common ancestor (LCA) (also called least common ancestor)
+of two nodes v and w in a tree or directed acyclic graph (DAG) T is the lowest (i.e. deepest) node that has both v and w as descendants,
+where we define each node to be a descendant of itself (so if v has a direct connection from w, w is the lowest common ancestor).
+"""
+
 import sys
 
 n = int(sys.stdin.readline())
