74fec948409f3b5fafff0a474a3b26bcf7144301
Jongmin Kim <kjongmin26@gmail.com>
2022-02-18 20:29:24 +0900

    Update fft.py

--- fft.py
+++ fft.py
@@ -52,3 +52,51 @@ def multiply(a, b):
     for i in range(n):
         c[i] = round(c[i].real / n)
     return c
+
+#--------------------------------------------------------------------------------
+
+def fft(arr, inv):
+    n = len(arr)
+    j = 0
+    for i in range(1, n):
+        bit = n // 2
+        while True:
+            j ^= bit
+            if j & bit:
+                break
+            bit //= 2
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    i = 1
+    while i < n:
+        if inv:
+            x = pi / i
+        else:
+            x = -pi / i
+        w = complex(cos(x), sin(x))
+        for j in range(0, n, 2 * i):
+            wk = complex(1, 0)
+            for k in range(i):
+                tmp = arr[i + j + k] * wk
+                arr[i + j + k] = arr[j + k] - tmp
+                arr[j + k] += tmp
+                wk *= w
+        i *= 2
+
+
+def multiply(a, b):
+    n = 1
+    while (n <= len(a)) or (n <= len(b)):
+        n *= 2
+    n *= 2
+
+    a.extend([0] * (n - len(a)))
+    b.extend([0] * (n - len(b)))
+    fft(a, False)
+    fft(b, False)
+    c = [a[i] * b[i] for i in range(n)]
+    fft(c, True)
+    for i in range(n):
+        c[i] = round(c[i].real / n)
+    return c
