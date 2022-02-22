3088c90bad0a1cffcbf7a4cceb0ce73897ddaa5c
Jongmin Kim <kjongmin26@gmail.com>
2022-02-22 19:38:01 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -1,1 +1,30 @@
 // BOJ 1978
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MAX = 1000;
+bool p[MAX + 1];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    fill_n(p, MAX + 1, 1);
+    p[0] = p[1] = 0;
+
+    for (int i = 2; i < MAX + 1; i++)
+        if (p[i])
+            for (int j = i * i; j < MAX + 1; j += i)
+                p[j] = 0;
+    
+
+    int q, n, ans = 0;
+    cin >> q;
+    while (q--) {
+        cin >> n;
+        ans += p[n];
+    }
+    cout << ans;
+}
