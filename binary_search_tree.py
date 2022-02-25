0b88c03651f5bc80b1e30fdb3168670aa3210021
Jongmin Kim <kjongmin26@gmail.com>
2022-02-25 20:52:10 +0900

    Update binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,13 +1,12 @@
 """
-In computer science, a binary search tree (BST), also called an ordered or sorted binary tree, is a rooted binary 
-tree whose internal nodes each store a key greater than all the keys in the node's left subtree and less than those 
-in its right subtree. A binary tree is a type of data structure for storing data such as numbers in an organized way. 
-Binary search trees allow binary search for fast lookup, addition and removal of data items, and can be used to 
-implement dynamic sets and lookup tables. The order of nodes in a BST means that each comparison skips about half of 
-the remaining tree, so the whole lookup takes time proportional to the binary logarithm of the number of items stored 
-in the tree. This is much better than the linear time required to find items by key in an (unsorted) array, 
-but slower than the corresponding operations on hash tables. Several variants of the binary search tree have been 
-studied.
+In computer science, a binary search tree (BST), also called an ordered or sorted binary tree, is a rooted binary tree
+whose internal nodes each store a key greater than all the keys in the node's left subtree and less than those in its right subtree.
+A binary tree is a type of data structure for storing data such as numbers in an organized way.
+Binary search trees allow binary search for fast lookup, addition and removal of data items, and can be used to implement dynamic sets and lookup tables.
+The order of nodes in a BST means that each comparison skips about half of the remaining tree,
+so the whole lookup takes time proportional to the binary logarithm of the number of items stored in the tree.
+This is much better than the linear time required to find items by key in an (unsorted) array, but slower than the corresponding operations on hash tables.
+Several variants of the binary search tree have been studied.
 """
 
 
