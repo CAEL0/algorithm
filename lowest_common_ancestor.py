459edb56f76cb0da455122a3f191386429c0ca4a
Jongmin Kim <kjongmin26@gmail.com>
2022-04-07 11:12:03 +0900

    Update lowest_common_ancestor.py

--- lowest_common_ancestor.py
+++ lowest_common_ancestor.py
@@ -4,6 +4,8 @@ of two nodes v and w in a tree or directed acyclic graph (DAG) T is the lowest (
 where we define each node to be a descendant of itself (so if v has a direct connection from w, w is the lowest common ancestor).
 """
 
+# BOJ 11438 LCA 2
+
 import sys
 
 n = int(sys.stdin.readline())
