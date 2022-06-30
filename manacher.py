2203c70566436d3461edf51f5c60eff584fc1ae0
Jongmin Kim <kjongmin26@gmail.com>
2022-07-01 01:13:56 +0900

    Create manacher.py

---
+++ manacher.py
@@ -0,0 +1,34 @@
+# BOJ 13275 가장 긴 팰린드롬 부분 문자열
+
+
+def manacher(s):
+    n = len(s)
+    a = [0] * n
+    r = p = -1
+    for i in range(1, n):
+        if r < i - 1 + a[i - 1]:
+            r = i - 1 + a[i - 1]
+            p = i - 1
+        
+        if r >= i:
+            a[i] = min(r - i, a[2 * p - i])
+        
+        while True:
+            a[i] += 1
+            if (i - a[i] == -1) or (i + a[i] == n):
+                a[i] -= 1
+                break
+            
+            if s[i - a[i]] != s[i + a[i]]:
+                a[i] -= 1
+                break
+    return max(a)
+
+
+s = input()
+ans = 2 * manacher(s) + 1
+ss = '#'
+for i in range(len(s)):
+    ss += s[i] + '#'
+ans = max(ans, manacher(ss))
+print(ans)
