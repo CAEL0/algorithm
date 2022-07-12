063da3f9e1aa0a24dc55a7533bd79c0d4dfe0a6e
Jongmin Kim <kjongmin26@gmail.com>
2022-07-12 11:44:52 +0900

    Update convex_hull_trick.py

--- convex_hull_trick.py
+++ convex_hull_trick.py
@@ -1,32 +1,27 @@
 # BOJ 13263 나무 자르기
 
-# import sys
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+a = [*map(int, input().split())]
+b = [*map(int, input().split())]
+stack = [(b[0], 0)]
 
-# def cross(j, k):
-#     x1, y1 = stack[j]
-#     x2, y2 = stack[k]
-#     return (y2 - y1) / (x1 - x2)
+for i in range(1, n):
+    left = 0
+    right = len(stack) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if stack[mid + 1][1] - stack[mid][1] <= a[i] * (stack[mid][0] - stack[mid + 1][0]):
+            left = mid + 1
+        else:
+            right = mid
+    
+    stack.append((b[i], stack[left][0] * a[i] + stack[left][1]))
+    while len(stack) > 2:
+        if (stack[-1][1] - stack[-2][1]) * (stack[-3][0] - stack[-2][0]) >= (stack[-2][1] - stack[-3][1]) * (stack[-2][0] - stack[-1][0]):
+            break
+        stack.pop(-2)
 
-
-# n = int(sys.stdin.readline())
-# a = list(map(int, sys.stdin.readline().split()))
-# b = list(map(int, sys.stdin.readline().split()))
-# stack = [(b[0], 0)]
-
-# for i in range(1, n):
-#     left = 0
-#     right = len(stack) - 1
-#     while left < right:
-#         mid = (left + right) // 2
-#         if cross(mid, mid + 1) <= a[i]:
-#             left = mid + 1
-#         else:
-#             right = mid
-#     stack.append((b[i], stack[left][0] * a[i] + stack[left][1]))
-#     while (len(stack) >= 3) and (cross(-2, -1) <= cross(-3, -2)):
-#         stack.pop(-2)
-
-# print(stack[-1][1])
-
-# need to be fixed
+print(stack[-1][1])
