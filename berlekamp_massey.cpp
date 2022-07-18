6c79b2944f348c8c9367575d69cb0e54e9c046af
Jongmin Kim <kjongmin26@gmail.com>
2022-07-18 21:01:22 +0900

    Update berlekamp_massey.cpp

--- berlekamp_massey.cpp
+++ berlekamp_massey.cpp
@@ -10,7 +10,7 @@ ll power(ll base, ll exp) {
 	ll res = 1;
 	while (exp) {
 		if (exp & 1)
-            res = res * base % MOD;
+            		res = res * base % MOD;
 		base = base * base % MOD;
 		exp >>= 1;
 	}
