62083165c63712cc069cc0b9b65771747ed06f5c
Jongmin Kim <kjongmin26@gmail.com>
2022-07-19 18:21:06 +0900

    Create matrix_multiplication.cpp

---
+++ matrix_multiplication.cpp
@@ -0,0 +1,64 @@
+// BOJ 17401 일하는 세포
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const int MOD = 1e9 + 7;
+int T, N, D;
+
+struct Matrix {
+    int m[25][25] = {0};
+    
+    Matrix operator* (const Matrix o) const {
+        Matrix res;
+        for (int i = 0; i < 25; i++)
+            for (int j = 0; j < 25; j++)
+                for (int k = 0; k < 25; k++)
+                    res.m[i][j] = (res.m[i][j] + (ll)m[i][k] * o.m[k][j]) % MOD;
+        return res;
+    }
+} graph[105], MQ, MR, ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> T >> N >> D;
+    for (int i = 0; i < T; i++) {
+        int m;
+        cin >> m;
+        while (m--) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            graph[i].m[--a][--b] = c;
+        }
+    }
+    int q = D / T;
+    int r = D % T;
+    for (int i = 0; i < N; i++)
+        MQ.m[i][i] = MR.m[i][i] = ans.m[i][i] = 1;
+    
+    for (int i = 0; i < T; i++) {
+        MQ = MQ * graph[i];
+        if (i < r)
+            MR = MR * graph[i];
+    }
+    while (q) {
+        if (q & 1)
+            ans = ans * MQ;
+        
+        q >>= 1;
+        MQ = MQ * MQ;
+    }
+    ans = ans * MR;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << ans.m[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
