a4fec1cce91c32cce28ea8c55f06b1bd65d8bd45
Jongmin Kim <kjongmin26@gmail.com>
2022-07-20 12:10:40 +0900

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+
+struct Node {
+    int idx[26];
+    bool finish;
+};
+struct Trie {
+    vector<Node> nodes{Node()};
+
+    void insert(const string s) {
+        int cur = 0;
+        for (char c: s) {
+            if (!nodes[cur].idx[c - 97]) {
+                nodes[cur].idx[c - 97] = nodes.size();
+                nodes.push_back(Node());
+            }
+            cur = nodes[cur].idx[c - 97];
+        }
+        nodes[cur].finish = true;
+    }
+    bool search(const string s) {
+        int cur = 0;
+        for (char c: s) {
+            if (!nodes[cur].idx[c - 97]) {
+                return false;
+            }
+            cur = nodes[cur].idx[c - 97];
+        }
+        return nodes[cur].finish;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    
+}
