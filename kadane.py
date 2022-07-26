9dfbb43930ee94654786a4b7aead393d0715a401
Jongmin Kim <kjongmin26@gmail.com>
2022-07-26 17:52:04 +0900

    Update kadane.py

--- kadane.py
+++ kadane.py
@@ -1,7 +1,10 @@
 # BOJ 1912 연속합
 
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = list(map(int, input().split()))
+arr = [*map(int, input().split())]
 ans = [arr[0]]
 for i in range(1, n):
     ans.append(max(ans[-1] + arr[i], arr[i]))
