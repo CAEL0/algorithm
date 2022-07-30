dbb78f93407fdfd052b6a813290f69d717b02c2c
Jongmin Kim <kjongmin26@gmail.com>
2022-07-30 14:09:56 +0900

    Update mcmf.py

--- mcmf.py
+++ mcmf.py
@@ -1,10 +1,11 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-a = list(map(int, sys.stdin.readline().split()))
-b = list(map(int, sys.stdin.readline().split()))
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+n, m = map(int, input().split())
+a = [*map(int, input().split())]
+b = [*map(int, input().split())]
+board = [list(map(int, input().split())) for _ in range(m)]
 graph = [[] for _ in range(m + n + 2)]
 
 for i in range(1, m + 1):
