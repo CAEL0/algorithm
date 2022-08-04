834bab794cdc42068144b92b1ebde08e85efd800
Jongmin Kim <kjongmin26@gmail.com>
2022-08-04 16:36:40 +0900

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,5 +1,3 @@
-// BOJ 2042 구간 합 구하기
-
 #include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
@@ -56,26 +54,3 @@ struct Fenwick {
         }
     }
 } fw;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    int M, K;
-    cin >> N >> M >> K;
-
-    for (int i = 1; i <= N; i++) {
-        ll v;
-        cin >> v;
-        fw.add(i, v);
-    }
-    int Q = M + K;
-    while (Q--) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        if (a == 1)
-            fw.update(b, c);
-        else
-            cout << fw.summation(b, c) << '\n';
-    }
-}
