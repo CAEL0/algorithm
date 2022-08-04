fad02de3c358839e6fac8ccb6b6db952cf664646
Jongmin Kim <kjongmin26@gmail.com>
2022-08-04 15:43:10 +0900

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -2,46 +2,64 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
 
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> pii;
 
-int n;
+int N;
 ll tree[2000010];
 
-ll summation(int k) {
-    ll res = 0;
-    while (k > 0) {
-        res += tree[k];
-        k -= (k & -k);
+struct Fenwick {
+    ll summation(int k) {
+        ll ret = 0;
+        while (k > 0) {
+            ret += tree[k];
+            k -= (k & -k);
+        }
+        return ret;
     }
-    return res;
-}
-void update(int k, ll c) {
-    while (k < 2 * n) {
-        tree[k] += c;
-        k += (k & -k);
+    ll summation(int l, int r) {
+        return summation(r) - summation(l - 1);
     }
-}
+    void update(int k, ll v) {
+        ll gap = v - summation(k, k);
+        while (k < 2 * N) {
+            tree[k] += gap;
+            k += (k & -k);
+        }
+    }
+    void add(int k, ll v) {
+        while (k < 2 * N) {
+            tree[k] += v;
+            k += (k & -k);
+        }
+    }
+} fw;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
-    int m, k;
-    cin >> n >> m >> k;
 
-    for (ll tmp, i = 1; i <= n; i++) {
-        cin >> tmp;
-        update(i, tmp);
+    int M, K;
+    cin >> N >> M >> K;
+
+    for (int i = 1; i <= N; i++) {
+        ll v;
+        cin >> v;
+        fw.add(i, v);
     }
-    ll a, b, c;
-    for (int i = 0; i < m + k; i++) {
+    int Q = M + K;
+    while (Q--) {
+        ll a, b, c;
         cin >> a >> b >> c;
-        if (a == 1) {
-            ll tmp = summation(b) - summation(b - 1);
-            update(b, c - tmp);
-        }
-        else cout << summation(c) - summation(b - 1) << '\n';
+        if (a == 1)
+            fw.update(b, c);
+        else
+            cout << fw.summation(b, c) << '\n';
     }
 }
