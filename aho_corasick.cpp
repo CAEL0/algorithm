d29d7c0de582405fda88b5f50c0f81b1c2c86e99
Jongmin Kim <kjongmin26@gmail.com>
2022-08-05 16:38:39 +0900

    Create aho_corasick.cpp

---
+++ aho_corasick.cpp
@@ -0,0 +1,111 @@
+// BOJ 9250 문자열 집합 판별
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int N, Q;
+char S[105];
+
+struct Trie {
+    Trie* go[26];
+    Trie* fail;
+    bool output;
+
+    Trie() {
+        fill_n(go, 26, nullptr);
+        output = false;
+    }
+    ~Trie() {
+        for (int i = 0; i < 26; i++)
+            if (go[i])
+                delete go[i];
+    }
+    void insert(const char* s) {
+        if (*s == '\0') {
+            output = true;
+            return;
+        }
+        int nxt = (*s - 'a');
+        if (!go[nxt])
+            go[nxt] = new Trie;
+        
+        go[nxt]->insert(s + 1);
+    }
+};
+Trie* root;
+
+void init() {
+    root = new Trie;
+    for (int i = 0; i < N; i++) {
+        cin >> S;
+        root->insert(S);
+    }
+    queue<Trie*> qu;
+    root->fail = root;
+    qu.push(root);
+    while (qu.sz) {
+        Trie* cur = qu.front();
+        qu.pop();
+        for (int i = 0; i < 26; i++) {
+            Trie* nxt = cur->go[i];
+            if (!nxt)
+                continue;
+            
+            if (cur == root)
+                nxt->fail = root;
+            else {
+                Trie *dst = cur->fail;
+                while (dst != root && !(dst->go[i]))
+                    dst = dst->fail;
+                
+                if (dst->go[i])
+                    dst = dst->go[i];
+                
+                nxt->fail = dst;
+            }
+            if (nxt->fail->output)
+                nxt->output = true;
+            
+            qu.push(nxt);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+    init();
+    
+    cin >> Q;
+    while (Q--) {
+        string T;
+        cin >> T;
+
+        Trie* cur = root;
+        bool flag = false;
+        for (int i = 0; i < T.length(); i++) {
+            int nxt = T[i] - 'a';
+            while (cur != root && !cur->go[nxt])
+                cur = cur->fail;
+            
+            if (cur->go[nxt])
+                cur = cur->go[nxt];
+            
+            if (cur->output) {
+                flag = true;
+                break;
+            }
+        }
+        cout << (flag ? "YES" : "NO") << '\n';
+    }
+}
