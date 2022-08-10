fc56247c1e86957c2d2569656570e3b69daf6d91
Jongmin Kim <kjongmin26@gmail.com>
2022-08-10 14:39:17 +0900

    Update euler_tour_technique.py

--- euler_tour_technique.py
+++ euler_tour_technique.py
@@ -9,12 +9,14 @@ It was introduced by Tarjan and Vishkin in 1984.
 # BOJ 14287 회사 문화 3
 
 import sys
+from math import log2, ceil
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
+n, m = map(int, input().split())
+num = [*map(int, input().split())]
 graph = [[] for _ in range(n + 1)]
 for i in range(1, n):
-    graph[arr[i]].append(i + 1)
+    graph[num[i]].append(i + 1)
 
 dfn = 0
 dfs_in = [0] * (n + 1)
@@ -27,7 +29,41 @@ def dfs(cur):
     dfs_in[cur] = dfn
     for pos in graph[cur]:
         dfs(pos)
+
     dfs_out[cur] = dfn
 
 
 dfs(1)
+height = ceil(log2(n))
+tree = [0] * 2 ** (height + 1)
+
+
+def partial_sum(a, b):
+    res = 0
+    a += 2 ** height - 1
+    b += 2 ** height - 1
+    while a <= b:
+        if a % 2:
+            res += tree[a]
+            a += 1
+        if b % 2 == 0:
+            res += tree[b]
+            b -= 1
+        a //= 2
+        b //= 2
+    return res
+
+
+def change(k, c):
+    idx = 2 ** height + k - 1
+    while idx >= 1:
+        tree[idx] += c
+        idx //= 2
+
+
+for _ in range(m):
+    query = [*map(int, input().split())]
+    if query[0] == 1:
+        change(dfs_in[query[1]], query[2])
+    else:
+        print(partial_sum(dfs_in[query[1]], dfs_out[query[1]]))
