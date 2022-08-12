14ea9ead4851f4e8afa0aae17c6781d0e28adfb1
Jongmin Kim <kjongmin26@gmail.com>
2022-08-12 17:41:26 +0900

    Update aho_corasick.cpp

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -109,3 +109,136 @@ int main() {
         cout << (flag ? "YES" : "NO") << '\n';
     }
 }
+
+//--------------------------------------------------------------------------------
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int N, Q;
+char P[105];
+string S;
+
+struct Trie {
+    vector<pair<char, Trie*>> go;
+    Trie* fail;
+    bool output;
+
+    Trie() {
+        output = false;
+    }
+    ~Trie() {
+        go.clear();
+    }
+    void insert(const char* s) {
+        if (*s == '\0') {
+            output = true;
+            return;
+        }
+        for (auto node: go) {
+            if (node.fi == *s) {
+                node.se->insert(s + 1);
+                return;
+            }
+        }
+        go.push_back({*s, new Trie});
+        go.bk.se->insert(s + 1);
+    }
+};
+Trie* root;
+
+void init() {
+    root = new Trie;
+    for (int i = 0; i < N; i++) {
+        cin >> P;
+        root->insert(P);
+    }
+    queue<Trie*> qu;
+    root->fail = root;
+    qu.push(root);
+    while (qu.sz) {
+        Trie* cur = qu.front();
+        qu.pop();
+        for (auto &node: cur->go) {
+            if (cur == root)
+                node.se->fail = root;
+            else {
+                Trie *dst = cur->fail;
+                while (dst != root) {
+                    bool flag = false;
+                    for (auto z: dst->go) {
+                        if (node.fi == z.fi) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag)
+                        break;
+                    
+                    dst = dst->fail;
+                }
+                for (auto z: dst->go) {
+                    if (node.fi == z.fi) {
+                        dst = z.se;
+                        break;
+                    }
+                }
+                node.se->fail = dst;
+            }
+            if (node.se->fail->output)
+                node.se->output = true;
+            
+            qu.push(node.se);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+    init();
+    
+    cin >> Q;
+    while (Q--) {
+        cin >> S;
+
+        Trie* cur = root;
+        bool ans = false;
+        for (char nxt: S) {
+            while (cur != root) {
+                bool flag = false;
+                for (auto z: cur->go) {
+                    if (nxt == z.fi) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                    break;
+                
+                cur = cur->fail;
+            }
+            for (auto z: cur->go) {
+                if (nxt == z.fi) {
+                    cur = z.se;
+                    break;
+                }
+            }
+            if (cur->output) {
+                ans = true;
+                break;
+            }
+        }
+        cout << (ans ? "YES" : "NO") << '\n';
+    }
+}
