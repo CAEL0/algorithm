cb8eecc33eec64be87e7fd06b919790f5af05da9
Jongmin Kim <kjongmin26@gmail.com>
2022-08-19 16:47:20 +0900

    Update persistent_segment_tree.cpp

--- persistent_segment_tree.cpp
+++ persistent_segment_tree.cpp
@@ -87,3 +87,91 @@ int main() {
         cout << ans << '\n';
     }
 }
+
+//--------------------------------------------------------------------------------
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+ 
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int MAX = 100005;
+int N, Q, node_idx, roots[MAX];
+vector<int> eggs[MAX];
+
+struct Node {
+    int v, l, r;
+} nodes[200005];
+
+int newNode(int v) {
+    nodes[++node_idx] = {v, 0, 0};
+    return node_idx;
+}
+int addNode(int prv, int s, int e, int i, int v) {
+    int cur = newNode(nodes[prv].v + v);
+    if (s != e) {
+        int m = (s + e) >> 1;
+        if (i <= m) {
+            nodes[cur].l = addNode(nodes[prv].l, s, m, i, v);
+            nodes[cur].r = nodes[prv].r;
+        } else {
+            nodes[cur].l = nodes[prv].l;
+            nodes[cur].r = addNode(nodes[prv].r, m + 1, e, i, v);
+        }
+    }
+    return cur;
+}
+int summation(int cur, int s, int e, int l, int r) {
+    if (r < s || e < l)
+        return 0;
+    
+    if (l <= s && e <= r)
+        return nodes[cur].v;
+    
+    int m = (s + e) >> 1;
+    return summation(nodes[cur].l, s, m, l, r) + summation(nodes[cur].r, m + 1, e, l, r);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int T;
+    cin >> T;
+    while (T--) {
+        cin >> N >> Q;
+        for (int x = 0; x < MAX; x++)
+            eggs[x].clear();
+        
+        for (int i = 1; i <= N; i++) {
+            int x, y;
+            cin >> x >> y;
+            eggs[x + 1].push_back(y + 1);
+        }
+        node_idx = 0;
+        roots[0] = 1;
+        newNode(0);
+        for (int x = 1; x < MAX; x++) {
+            roots[x] = roots[x - 1];
+            for (int y: eggs[x]) {
+                int tmp = node_idx + 1;
+                addNode(roots[x], 1, MAX, y, 1);
+                roots[x] = tmp;
+            }
+        }
+        int ans = 0;
+        while (Q--) {
+            int l, r, b, t;
+            cin >> l >> r >> b >> t;
+            ans += summation(roots[r + 1], 1, MAX, b + 1, t + 1);
+            ans -= summation(roots[l], 1, MAX, b + 1, t + 1);
+        }
+        cout << ans << '\n';
+    }
+}
