9140431f487e3b19594df0f228e457b0e00c96ed
Jongmin Kim <kjongmin26@gmail.com>
2022-08-19 12:40:32 +0900

    Update lowest_common_ancestor.cpp

--- lowest_common_ancestor.cpp
+++ lowest_common_ancestor.cpp
@@ -2,54 +2,48 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
 
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> pii;
 
-int N, Q;
-vector<int> graph[100005];
-bool vst[100005];
-int depth[100005], sparse[20][100005];
+const int MAX = 100005;
+int N, Q, depth[MAX], sparse[20][MAX];
+vector<int> graph[MAX];
 
-struct Node {
-    int idx, d;
-};
+void dfs(int cur, int d) {
+    depth[cur] = d;
+    for (int nxt: graph[cur]) {
+        if (!depth[nxt]) {
+            sparse[0][nxt] = cur;
+            dfs(nxt, d + 1);
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> N;
-
-    int a, b, c;
-    for (int i = 0; i < N - 1; i++) {
+    for (int i = 1; i < N; i++) {
+        int a, b;
         cin >> a >> b;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-    stack<Node> st;
-    st.push({1, 2});
-    vst[1] = true;
-    depth[1] = 1;
-    while (st.size()) {
-        Node cur = st.top();
-        st.pop();
-        for (int nxt: graph[cur.idx]) {
-            if (!vst[nxt]) {
-                vst[nxt] = true;
-                depth[nxt] = cur.d;
-                sparse[0][nxt] = cur.idx;
-                st.push({nxt, cur.d + 1});
-            }
-        }
-    }
+    dfs(1, 1);
     for (int i = 0; i < 19; i++)
         for (int j = 1; j <= N; j++)
             sparse[i + 1][j] = sparse[i][sparse[i][j]];
     
     cin >> Q;
-
     while (Q--) {
+        int a, b;
         cin >> a >> b;
 
         if (depth[a] > depth[b])
@@ -60,20 +54,20 @@ int main() {
             if (d & (1 << j))
                 b = sparse[j][b];
         
-        if (a == b) {
+        if (a == b)
             cout << a << '\n';
-            continue;
-        }
-        while (1) {
-            for (int j = 19; j >= 0; j--) {
-                if (sparse[j][a] != sparse[j][b]) {
-                    a = sparse[j][a];
-                    b = sparse[j][b];
+        else {
+            while (1) {
+                for (int j = 19; j >= 0; j--) {
+                    if (sparse[j][a] != sparse[j][b]) {
+                        a = sparse[j][a];
+                        b = sparse[j][b];
+                    }
                 }
+                if (sparse[0][a] == sparse[0][b])
+                    cout << sparse[0][a] << '\n';
+                    break;
             }
-            if (sparse[0][a] == sparse[0][b])
-                cout << sparse[0][a] << '\n';
-                break;
         }
     }
 }
