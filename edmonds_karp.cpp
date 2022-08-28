a58ba0c5eb4c24e92fb364b4b22780f493171742
Jongmin Kim <kjongmin26@gmail.com>
2022-08-28 16:51:13 +0900

    Update edmonds_karp.cpp

--- edmonds_karp.cpp
+++ edmonds_karp.cpp
@@ -2,8 +2,14 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
 
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -58,6 +64,81 @@ int main() {
                 }
             }
             graph[nxt].push_back(cur);
+            nxt = cur;
+        }
+    }
+    cout << ans;
+}
+
+//--------------------------------------------------------------------------------
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int MAX = 405;
+const int INF = 2e9;
+int N, M, K, prv[MAX], G[MAX][MAX];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+    K = N + M + 1;
+    for (int i = 1; i <= N; i++) {
+        G[0][i]++;
+        int tmp;
+        cin >> tmp;
+        while (tmp--) {
+            int j;
+            cin >> j;
+            G[i][N + j]++;
+        }
+    }
+    for (int j = N + 1; j < K; j++)
+        G[j][K]++;
+    
+    int ans = 0;
+    while (1) {
+        memset(prv, -1, sizeof(prv));
+        deque<pii> dq;
+        dq.push_back(pii(0, INF));
+        int res = INF;
+        while (dq.sz) {
+            pii cur = dq.front();
+            dq.pop_front();
+
+            if (G[cur.fi][K]) {
+                res = min(res, G[cur.fi][K]);
+                prv[K] = cur.fi;
+                break;
+            }
+            for (int nxt = 1; nxt < K; nxt++) {
+                if (prv[nxt] == -1 && G[cur.fi][nxt]) {
+                    prv[nxt] = cur.fi;
+                    dq.push_back(pii(nxt, min(res, G[cur.fi][nxt])));
+                }
+            }
+            if (prv[K] != -1)
+                break;
+        }
+        if (prv[K] == -1)
+            break;
+        
+        ans += res;
+        int nxt = K;
+        while (nxt) {
+            int cur = prv[nxt];
+            G[cur][nxt] -= res;
+            G[nxt][cur] += res;
             nxt = cur;
         }
     }
