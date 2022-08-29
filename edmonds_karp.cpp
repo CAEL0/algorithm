b58f5d0832a34e48c89016296f7748dbb05575de
Jongmin Kim <kjongmin26@gmail.com>
2022-08-29 13:59:03 +0900

    Update edmonds_karp.cpp

--- edmonds_karp.cpp
+++ edmonds_karp.cpp
@@ -11,134 +11,71 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    int n, m, k;
-    cin >> n >> m;
-    k = n + m + 2;
-    vector<int> graph[k];
-    
-    for (int j, tmp, i = 1; i < n + 1; i++) {
-        graph[0].push_back(i);
-        cin >> j;
-        while (j--) {
-            cin >> tmp;
-            graph[i].push_back(n + tmp);
-        }
-    }
-    for (int i = n + 1; i < k - 1; i++)
-        graph[i].push_back(k - 1);
-
-    int ans = 0;
-    while (true) {
-        int prev[k];
-        fill_n(prev, k, -1);
-        deque<int> dq;
-        dq.push_back(0);
-        while (!dq.empty()) {
-            int cur = dq.front();
-            dq.pop_front();
-            for (int nxt : graph[cur]) {
-                if (nxt == k - 1) {
-                    prev[k - 1] = cur;
-                    break;
-                }
-                if (prev[nxt] == -1) {
-                    prev[nxt] = cur;
-                    dq.push_back(nxt);
-                }
-            }
-            if (prev[k - 1] != -1) break;
-        }
-        if (prev[k - 1] == -1) break;
-        ans++;
-        int nxt = k - 1;
-        while (nxt) {
-            int cur = prev[nxt];
-            for (int i = 0; i < graph[cur].size(); i++) {
-                if (graph[cur][i] == nxt) {
-                    graph[cur].erase(graph[cur].begin() + i);
-                    break;
-                }
-            }
-            graph[nxt].push_back(cur);
-            nxt = cur;
-        }
-    }
-    cout << ans;
-}
-
-//--------------------------------------------------------------------------------
-
-#include <iostream>
-#include <bits/stdc++.h>
-#define sz size()
-#define bk back()
-#define fi first
-#define se second
-
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-
 const int MAX = 405;
 const int INF = 2e9;
-int N, M, K, prv[MAX], G[MAX][MAX];
+const int S = MAX - 2;
+const int E = MAX - 1;
+int N, M, C[MAX][MAX], F[MAX][MAX];
+vector<int> G[MAX];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> N >> M;
-    K = N + M + 1;
     for (int i = 1; i <= N; i++) {
-        G[0][i]++;
+        G[S].push_back(i);
+        G[i].push_back(S);
+        C[S][i] = 1;
+
         int tmp;
         cin >> tmp;
         while (tmp--) {
             int j;
             cin >> j;
-            G[i][N + j]++;
+            G[i].push_back(N + j);
+            G[N + j].push_back(i);
+            C[i][N + j] = 1;
         }
     }
-    for (int j = N + 1; j < K; j++)
-        G[j][K]++;
-    
+    for (int j = N + 1; j <= N + M; j++) {
+        G[j].push_back(E);
+        G[E].push_back(j);
+        C[j][E] = 1;
+    }
     int ans = 0;
     while (1) {
+        int prv[MAX];
         memset(prv, -1, sizeof(prv));
-        deque<pii> dq;
-        dq.push_back(pii(0, INF));
-        int res = INF;
-        while (dq.sz) {
-            pii cur = dq.front();
-            dq.pop_front();
-
-            if (G[cur.fi][K]) {
-                res = min(res, G[cur.fi][K]);
-                prv[K] = cur.fi;
-                break;
-            }
-            for (int nxt = 1; nxt < K; nxt++) {
-                if (prv[nxt] == -1 && G[cur.fi][nxt]) {
-                    prv[nxt] = cur.fi;
-                    dq.push_back(pii(nxt, min(res, G[cur.fi][nxt])));
+        queue<int> Q({S});
+        while (Q.sz && prv[E] == -1) {
+            int cur = Q.front();
+            Q.pop();
+            for (int nxt: G[cur]) {
+                if (C[cur][nxt] > F[cur][nxt] && prv[nxt] == -1) {
+                    prv[nxt] = cur;
+                    Q.push(nxt);
+                    if (nxt == E)
+                        break;
                 }
             }
-            if (prv[K] != -1)
-                break;
         }
-        if (prv[K] == -1)
+        if (prv[E] == -1)
             break;
         
-        ans += res;
-        int nxt = K;
-        while (nxt) {
+        int flow = INF;
+        int nxt = E;
+        while (nxt != S) {
             int cur = prv[nxt];
-            G[cur][nxt] -= res;
-            G[nxt][cur] += res;
+            flow = min(flow, C[cur][nxt] - F[cur][nxt]);
+            nxt = cur;
+        }
+        ans += flow;
+        nxt = E;
+        while (nxt != S) {
+            int cur = prv[nxt];
+            F[cur][nxt] += flow;
+            F[nxt][cur] -= flow;
             nxt = cur;
         }
     }
