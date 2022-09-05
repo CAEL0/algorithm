c1088f6654168eda89b2dfabab072c5067ee57b1
Jongmin Kim <kjongmin26@gmail.com>
2022-09-05 16:44:47 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,33 +2,36 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
 #define fi first
 #define se second
-
+ 
 using namespace std;
+typedef long long ll;
 typedef pair<int, int> pii;
 
-vector<pii> graph[20001];
-int res[20001];
+const int MAX = 20005;
+int V, E, S, dist[MAX];
+struct Node {
+    int x, d;
+    bool operator<(const Node &o) const {
+        return d > o.d;
+    }
+};
+vector<pii> G[MAX];
 
-void dijkstra(int start) {
-    priority_queue<pii> pq;
-    pq.push(pii(0, start));
-
-    while (!pq.empty()) {
-        int old_weight, old_node;
-        tie(old_weight, old_node) = pq.top();
+void dijkstra() {
+    priority_queue<Node> pq;
+    pq.push({S, 0});
+    while (pq.sz) {
+        Node cur = pq.top();
         pq.pop();
-
-        if (res[old_node] >= -old_weight) {
-            for (auto new_item : graph[old_node]) {
-                int new_node, new_weight, total_weight;
-                tie(new_node, new_weight) = new_item;
-                total_weight = -old_weight + new_weight;
-
-                if (total_weight < res[new_node]) {
-                    res[new_node] = total_weight;
-                    pq.push(pii(-total_weight, new_node));
+        if (dist[cur.x] == cur.d) {
+            for (pii nxt: G[cur.x]) {
+                if (cur.d + nxt.se < dist[nxt.fi]) {
+                    dist[nxt.fi] = cur.d + nxt.se;
+                    pq.push({nxt.fi, dist[nxt.fi]});
                 }
             }
         }
@@ -39,22 +42,19 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    int v, e, start;
-    cin >> v >> e >> start;
-
-    for (int a, b, c, i = 0; i < e; i++) {
+    cin >> V >> E >> S;
+    while (E--) {
+        int a, b, c;
         cin >> a >> b >> c;
-        graph[a].push_back(pii(b, c));
+        G[a].push_back(pii(b, c));
     }
-    fill_n(res, v + 1, 3000001);
-    res[start] = 0;
-
-    dijkstra(start);
-    
-    for (int i = 1; i < v + 1; i++) {
-        if (res[i] == 3000001)
-            cout << "INF\n";
+    fill_n(dist, V + 1, 2e9);
+    dist[S] = 0;
+    dijkstra();
+    for (int i = 1; i <= V; i++) {
+        if (dist[i] == 2e9)
+            cout << "INF" << '\n';
         else
-            cout << res[i] << '\n';
+            cout << dist[i] << '\n';
     }
 }
