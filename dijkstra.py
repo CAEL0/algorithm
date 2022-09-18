eb21395cfa5e56687fe202e39139b013c477dd2e
Jongmin Kim <kjongmin26@gmail.com>
2022-09-18 13:11:02 +0900

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,10 +1,3 @@
-"""
-Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks.
-It was conceived by computer scientist Edsger W. Dijkstra in 1956 and published three years later. The algorithm exists in many variants.
-Dijkstra's original algorithm found the shortest path between two given nodes, but a more common variant fixes a single node as the "source" node
-and finds shortest paths from the source to all other nodes in the graph, producing a shortest-path tree.
-"""
-
 # BOJ 1753 최단경로
 
 import sys
