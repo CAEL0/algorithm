66a56d4def1a5cf924736144aa3b5be1dfad0433
Jongmin Kim <kjongmin26@gmail.com>
2022-09-20 13:02:30 +0900

    Update ford_fulkerson.py

--- ford_fulkerson.py
+++ ford_fulkerson.py
@@ -1,10 +1,3 @@
-"""
-The Ford–Fulkerson method or Ford–Fulkerson algorithm (FFA) is a greedy algorithm that computes the maximum flow in a flow network.
-It is sometimes called a "method" instead of an "algorithm" as the approach to finding augmenting paths in a residual graph is not fully specified 
-or it is specified in several implementations with different running times. It was published in 1956 by L. R. Ford Jr. and D. R. Fulkerson.
-The name "Ford–Fulkerson" is often also used for the Edmonds–Karp algorithm, which is a fully defined implementation of the Ford–Fulkerson method.
-"""
-
 import sys
 input = sys.stdin.readline
 
