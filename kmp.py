0e716d38367931fcb2be80fd20365da8218c3e82
Jongmin Kim <kjongmin26@gmail.com>
2022-09-29 16:26:12 +0900

    Update kmp.py

--- kmp.py
+++ kmp.py
@@ -1,9 +1,3 @@
-"""
-In computer science, the Knuth–Morris–Pratt string-searching algorithm (or KMP algorithm) searches for occurrences of a "word" W within a main "text string" S
-by employing the observation that when a mismatch occurs, the word itself embodies sufficient information to determine where the next match could begin,
-thus bypassing re-examination of previously matched characters.
-"""
-
 # BOJ 1786 찾기
 
 t = input()
