b7830e98e893468c4caf8a9057f4de79d4374fe9
Jongmin Kim <kjongmin26@gmail.com>
2022-10-08 19:55:02 +0900

    Update trie.py

--- trie.py
+++ trie.py
@@ -1,11 +1,3 @@
-"""
-In computer science, a trie, also called digital tree or prefix tree, is a type of k-ary search tree,
-a tree data structure used for locating specific keys from within a set.
-These keys are most often strings, with links between nodes defined not by the entire key, but by individual characters.
-In order to access a key (to recover its value, change it, or remove it), the trie is traversed depth-first,
-following the links between nodes, which represent each character in the key.
-"""
-
 class Node:
     def __init__(self, key, data=None):
         self.key = key
