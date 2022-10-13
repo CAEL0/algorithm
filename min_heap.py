7736f691f7f1d7f831b5b3c8eee05794287fa7a8
Jongmin Kim <kjongmin26@gmail.com>
2022-10-13 10:59:50 +0900

    Update min_heap.py

--- min_heap.py
+++ min_heap.py
@@ -1,12 +1,3 @@
-"""
-In computer science, a heap is a specialized tree-based data structure which is essentially an almost complete 
-tree that satisfies the heap property: in a max heap, for any given node C, if P is a parent node of C, then the key 
-(the value) of P is greater than or equal to the key of C.
-In a min heap, the key of P is less than or equal to the key of C.
-The node at the "top" of the heap (with no parents) is called the root node.
-"""
-
-
 class MinHeap:
     def __init__(self):
         self.array = [0]
