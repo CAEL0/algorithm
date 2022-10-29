83e861f87a1734ceef2a97df976b6ba29298f238
Jongmin Kim <kjongmin26@gmail.com>
2022-10-29 16:20:50 +0900

    Update tarjan.py

--- tarjan.py
+++ tarjan.py
@@ -1,10 +1,11 @@
 import sys
 sys.setrecursionlimit(10 ** 5)
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
 
 scc = []
