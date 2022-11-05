abf7da2cf6c68ac514d678438e030ec501d827fd
Jongmin Kim <kjongmin26@gmail.com>
2022-11-05 17:10:56 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -2,21 +2,29 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
 
 using namespace std;
-const int MAX = 1000;
-bool p[MAX + 1];
+typedef long long ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+const int MAX = 1000005;
+bool p[MAX];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    fill_n(p, MAX + 1, 1);
+    fill_n(p, MAX, 1);
     p[0] = p[1] = 0;
 
-    for (int i = 2; i < MAX + 1; i++)
+    for (int i = 2; i < MAX; i++)
         if (p[i])
-            for (int j = i * i; j < MAX + 1; j += i)
+            for (int j = i * i; j < MAX; j += i)
                 p[j] = 0;
     
 
