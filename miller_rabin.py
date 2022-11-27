16f5dfeebe2524b066e00ee5a6e2dc72d9ff8361
Jongmin Kim <kjongmin26@gmail.com>
2022-11-27 15:16:08 +0900

    Update miller_rabin.py

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,10 +1,16 @@
+# BOJ 5615 아파트 임대
+
+import sys
+input = sys.stdin.readline
+
+
 def miller_rabin(x):
     if x == 1:
         return False
 
     s = 0
     d = x - 1
-    while not d & 1:
+    while not (d & 1):
         s += 1
         d >>= 1
 
@@ -18,7 +24,7 @@ def miller_rabin(x):
 
         if s == 1:
             return False
-
+        
         for _ in range(s - 1):
             b = b * b % x
             if b == x - 1:
@@ -26,3 +32,10 @@ def miller_rabin(x):
         else:
             return False
     return True
+
+
+ans = 0
+for _ in range(int(input())):
+    ans += miller_rabin(2 * int(input()) + 1)
+
+print(ans)
