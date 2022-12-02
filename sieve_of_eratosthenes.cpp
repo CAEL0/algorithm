3d74d017b84c6d40bae14a24fb2a0272f820400d
Jongmin Kim <kjongmin26@gmail.com>
2022-12-02 16:06:47 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -12,26 +12,26 @@ typedef long long ll;
 typedef pair<int, int> pii;
 
 const int MAX = 1000005;
-bool p[MAX];
+bool P[MAX];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    fill_n(p, MAX, 1);
-    p[0] = p[1] = 0;
+    fill_n(P, MAX, 1);
+    P[0] = P[1] = 0;
 
     for (int i = 2; i < MAX; i++)
-        if (p[i])
+        if (P[i])
             for (int j = i * i; j < MAX; j += i)
-                p[j] = 0;
+                P[j] = 0;
     
 
     int q, n, ans = 0;
     cin >> q;
     while (q--) {
         cin >> n;
-        ans += p[n];
+        ans += P[n];
     }
     cout << ans;
 }
