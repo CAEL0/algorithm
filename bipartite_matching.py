2ffc828513da45af39bc6d6ccbe623bd5519c608
Jongmin Kim <kjongmin26@gmail.com>
2022-12-14 12:19:43 +0900

    Update bipartite_matching.py

--- bipartite_matching.py
+++ bipartite_matching.py
@@ -5,27 +5,27 @@ input = sys.stdin.readline
 
 
 def dfs(cur):
-    visit[cur] = 1
-    for nxt in graph[cur]:
-        if not b[nxt] or (not visit[b[nxt]] and dfs(b[nxt])):
-            b[nxt] = cur
+    vst[cur] = 1
+    for nxt in G[cur]:
+        if not B[nxt] or (not vst[B[nxt]] and dfs(B[nxt])):
+            B[nxt] = cur
             return 1
     return 0
 
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+N, M = map(int, input().split())
+G = [[] for _ in range(N + 1)]
 
-for i in range(1, n + 1):
-    data = [*map(int, input().split())]
-    for j in range(1, data[0] + 1):
-        graph[i].append(data[j])
+for i in range(1, N + 1):
+    arr = [*map(int, input().split())]
+    for j in range(1, arr[0] + 1):
+        G[i].append(arr[j])
 
-b = [0] * (m + 1)
+B = [0] * (M + 1)
 ans = 0
 
-for i in range(1, n + 1):
-    visit = [0] * (n + 1)
+for i in range(1, N + 1):
+    vst = [0] * (N + 1)
     ans += dfs(i)
 
 print(ans)
