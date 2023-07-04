9f2bcbfafadcbeb7a6abfa34b41bcde8b359da51
Jongmin Kim <kjongmin26@gmail.com>
2023-07-04 12:23:25 +0900

    Update convex_hull_trick.cpp

--- convex_hull_trick.cpp
+++ convex_hull_trick.cpp
@@ -1,6 +1,5 @@
 // BOJ 13263 나무 자르기
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define fi first
 #define se second
@@ -13,42 +12,43 @@ typedef long double ld;
 typedef pair<ll, ll> pll;
 
 const int MAX = 100005;
-int N, A[MAX], B[MAX];
-vector<pll> S;
+int n, a[MAX], b[MAX];
+vector<pll> stk;
 
-ld cross(int j, int k) {
-    auto [x, y] = S[j];
-    auto [z, w] = S[k];
+ld cross(int i, int j) {
+    auto [x, y] = stk[i];
+    auto [z, w] = stk[j];
     return (ld)(w - y) / (x - z);
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N;
+    cin >> n;
 
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
     
-    for (int i = 0; i < N; i++)
-        cin >> B[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
     
-    S.push_back(pll(B[0], 0));
+    stk.push_back(pll(b[0], 0));
     
-    for (int i = 1; i < N; i++) {
+    for (int i = 1; i < n; i++) {
         int left = 0;
-        int right = S.sz - 1;
+        int right = stk.sz - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (cross(mid, mid + 1) <= A[i])
+            if (cross(mid, mid + 1) <= a[i])
                 left = mid + 1;
             else
                 right = mid;
         }
-        S.push_back(pll(B[i], S[left].fi * A[i] + S[left].se));
-        while (S.sz > 2 && cross(S.sz - 2, S.sz - 1) <= cross(S.sz - 3, S.sz - 2))
-            S.erase(S.end() - 2);
+        stk.push_back(pll(b[i], stk[left].fi * a[i] + stk[left].se));
+        while (stk.sz > 2 && cross(stk.sz - 2, stk.sz - 1) <= cross(stk.sz - 3, stk.sz - 2))
+            stk.erase(stk.end() - 2);
     }
-    cout << S.bk.se;
+    cout << stk.bk.se;
 }
