7b888d0a9b394b5189d58d8b9928a92a781079d2
Jongmin Kim <kjongmin26@gmail.com>
2023-07-12 17:55:16 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -1,6 +1,5 @@
 // BOJ 1978 소수 찾기
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -12,26 +11,28 @@ typedef long long ll;
 typedef pair<int, int> pii;
 
 const int MAX = 1000005;
-bool P[MAX];
+bool sieve[MAX];
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    fill_n(P, MAX, 1);
-    P[0] = P[1] = 0;
-
-    for (ll i = 2; i < MAX; i++)
-        if (P[i])
-            for (ll j = i * i; j < MAX; j += i)
-                P[j] = 0;
+    cin.tie(0);
+    cout.tie(0);
     
-
-    int q, n, ans = 0;
+    fill_n(sieve, MAX, true);
+    sieve[0] = sieve[1] = false;
+    for (int i = 2; i < MAX; i++)
+        if (sieve[i])
+            for (ll j = (ll)i * i; j < MAX; j += i)
+                sieve[j] = false;
+    
+    int q;
     cin >> q;
+    
+    int ans = 0;
     while (q--) {
+        int n;
         cin >> n;
-        ans += P[n];
+        ans += sieve[n];
     }
     cout << ans;
 }
