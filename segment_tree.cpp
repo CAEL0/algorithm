19c535044a06a33c3b87dc029eeb49098b895dad
Jongmin Kim <kjongmin26@gmail.com>
2023-07-14 15:49:23 +0900

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,6 +1,5 @@
 //BOJ 2042 구간 합 구하기
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,83 +10,98 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int N, M, K;
-ll tree[3000010];
+const int MAX = 1000005;
 
-ll init(int idx, int s, int e) {
-    if (s == e) {
-        cin >> tree[idx];
-        return tree[idx];
+struct SegmentTree {
+    ll tree[4 * MAX];
+
+    ll init(int idx, int s, int e) {
+        if (s == e) {
+            cin >> tree[idx];
+            return tree[idx];
+        }
+        int m = (s + e) >> 1;
+        return tree[idx] = init(2 * idx, s, m) + init(2 * idx + 1, m + 1, e);
     }
-    int m = (s + e) >> 1;
-    return tree[idx] = init(2 * idx, s, m) + init(2 * idx + 1, m + 1, e);
-}
-ll summation(int idx, int s, int e, int l, int r) {
-    if (r < s || e < l)
-        return 0;
-    
-    if (l <= s && e <= r)
-        return tree[idx];
-    
-    int m = (s + e) >> 1;
-    return summation(2 * idx, s, m, l, r) + summation(2 * idx + 1, m + 1, e, l, r);
-}
-void update(int idx, int s, int e, int l, ll v) {
-    if (l < s || e < l)
-        return;
-    
-    if (s == e) {
-        tree[idx] = v;
-        return;
+
+    ll summation(int idx, int s, int e, int l, int r) {
+        if (r < s || e < l)
+            return 0;
+
+        if (l <= s && e <= r)
+            return tree[idx];
+
+        int m = (s + e) >> 1;
+        return summation(2 * idx, s, m, l, r) +
+               summation(2 * idx + 1, m + 1, e, l, r);
     }
-    int m = (s + e) >> 1;
-    update(2 * idx, s, m, l, v);
-    update(2 * idx + 1, m + 1, e, l, v);
-    tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
-}
+
+    void update(int idx, int s, int e, int l, ll v) {
+        if (l < s || e < l)
+            return;
+
+        if (s == e) {
+            tree[idx] = v;
+            return;
+        }
+        int m = (s + e) >> 1;
+        update(2 * idx, s, m, l, v);
+        update(2 * idx + 1, m + 1, e, l, v);
+        tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M >> K;
+    SegmentTree st = SegmentTree();
 
-    init(1, 1, N);
-    
-    for (int i = 0; i < M + K; i++) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        if (a == 1)
-            update(1, 1, N, b, c);
+    int n, p, q;
+    cin >> n >> p >> q;
+
+    st.init(1, 1, n);
+
+    q += p;
+    while (q--) {
+        ll op, x, y;
+        cin >> op >> x >> y;
+        if (op == 1)
+            st.update(1, 1, n, x, y);
         else
-            cout << summation(1, 1, N, b, c) << '\n';
+            cout << st.summation(1, 1, n, x, y) << '\n';
     }
 }
 
 //--------------------------------------------------------------------------------
 
-#include <iostream>
 #include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
 
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> pii;
 
 int powh;
 ll tree[3000010];
 
 ll summation(int a, int b) {
-    ll res = 0;
+    ll ret = 0;
     a += powh - 1;
     b += powh - 1;
     while (a <= b) {
         if (a & 1)
-            res += tree[a++];
+            ret += tree[a++];
         if (!(b & 1))
-            res += tree[b--];
+            ret += tree[b--];
         a >>= 1;
         b >>= 1;
     }
-    return res;
+    return ret;
 }
 void update(int k, ll c) {
     int idx = powh + k - 1;
@@ -100,7 +114,8 @@ void update(int k, ll c) {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
     int n, x, y;
     cin >> n >> x >> y;
