bd6d50dfe50bf32b474e6636a6548a1c675b397b
Jongmin Kim <kjongmin26@gmail.com>
2024-04-14 14:56:29 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,58 +1,66 @@
 // BOJ 1753 최단경로
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
 #define fi first
 #define se second
- 
+
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 20005;
-int V, E, S, dist[MAX];
 struct Node {
     int x, d;
-    bool operator<(const Node &o) const {
-        return d > o.d;
-    }
+    bool operator<(const Node &o) const { return d > o.d; }
 };
-vector<pii> G[MAX];
 
-void dijkstra() {
+vector<int> dijkstra(int start, vector<vector<pii>> &graph) {
+    vector<int> ret(graph.sz, INT_MAX);
+    ret[start] = 0;
+
     priority_queue<Node> pq;
-    pq.push({S, 0});
+    pq.push({start, 0});
+
     while (pq.sz) {
         Node cur = pq.top();
         pq.pop();
-        if (dist[cur.x] == cur.d) {
-            for (pii nxt: G[cur.x]) {
-                if (cur.d + nxt.se < dist[nxt.fi]) {
-                    dist[nxt.fi] = cur.d + nxt.se;
-                    pq.push({nxt.fi, dist[nxt.fi]});
-                }
+
+        if (ret[cur.x] != cur.d)
+            continue;
+
+        for (pii &nxt : graph[cur.x]) {
+            if (cur.d + nxt.se < ret[nxt.fi]) {
+                ret[nxt.fi] = cur.d + nxt.se;
+                pq.push({nxt.fi, ret[nxt.fi]});
             }
         }
     }
+
+    return ret;
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> V >> E >> S;
-    while (E--) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        G[a].push_back(pii(b, c));
+    int n, m, start;
+    cin >> n >> m >> start;
+
+    vector<vector<pii>> graph(n + 1);
+
+    while (m--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        graph[u].push_back(pii(v, w));
     }
-    fill_n(dist, V + 1, 2e9);
-    dist[S] = 0;
-    dijkstra();
-    for (int i = 1; i <= V; i++) {
-        if (dist[i] == 2e9)
+
+    vector<int> dist = dijkstra(start, graph);
+
+    for (int i = 1; i <= n; i++) {
+        if (dist[i] == INT_MAX)
             cout << "INF" << '\n';
         else
             cout << dist[i] << '\n';
