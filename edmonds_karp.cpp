66de717743ae15f3032d8364114932c9219701a9
Jongmin Kim <kjongmin26@gmail.com>
2024-04-19 20:33:18 +0900

    Update edmonds_karp.cpp

--- edmonds_karp.cpp
+++ edmonds_karp.cpp
@@ -1,6 +1,5 @@
 // BOJ 2188 축사 배정
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,71 +10,86 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 405;
-const int INF = 2e9;
-const int S = MAX - 2;
-const int E = MAX - 1;
-int N, M, C[MAX][MAX], F[MAX][MAX];
-vector<int> G[MAX];
-
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        G[S].push_back(i);
-        G[i].push_back(S);
-        C[S][i] = 1;
+    int n, m;
+    cin >> n >> m;
+
+    int mx = n + m + 3;
+    int start = mx - 2;
+    int end = mx - 1;
+
+    vector<vector<int>> graph(mx);
+    vector<vector<int>> capacity(mx, vector<int>(mx, 0));
+    vector<vector<int>> flow(mx, vector<int>(mx, 0));
+
+    for (int i = 1; i <= n; i++) {
+        graph[start].push_back(i);
+        graph[i].push_back(start);
+        capacity[start][i] = 1;
 
         int tmp;
         cin >> tmp;
+
         while (tmp--) {
             int j;
             cin >> j;
-            G[i].push_back(N + j);
-            G[N + j].push_back(i);
-            C[i][N + j] = 1;
+
+            graph[i].push_back(n + j);
+            graph[n + j].push_back(i);
+            capacity[i][n + j] = 1;
         }
     }
-    for (int j = N + 1; j <= N + M; j++) {
-        G[j].push_back(E);
-        G[E].push_back(j);
-        C[j][E] = 1;
+
+    for (int j = n + 1; j <= n + m; j++) {
+        graph[j].push_back(end);
+        graph[end].push_back(j);
+        capacity[j][end] = 1;
     }
+
     int ans = 0;
+
     while (1) {
-        int prv[MAX];
-        memset(prv, -1, sizeof(prv));
-        queue<int> Q({S});
-        while (Q.sz && prv[E] == -1) {
-            int cur = Q.front();
-            Q.pop();
-            for (int nxt: G[cur]) {
-                if (C[cur][nxt] > F[cur][nxt] && prv[nxt] == -1) {
+        vector<int> prv(mx, -1);
+        deque<int> dq({start});
+
+        while (dq.sz && prv[end] == -1) {
+            int cur = dq.front();
+            dq.pop_front();
+
+            for (int nxt : graph[cur]) {
+                if (capacity[cur][nxt] > flow[cur][nxt] && prv[nxt] == -1) {
                     prv[nxt] = cur;
-                    Q.push(nxt);
-                    if (nxt == E)
+                    dq.push_back(nxt);
+
+                    if (nxt == end)
                         break;
                 }
             }
         }
-        if (prv[E] == -1)
+
+        if (prv[end] == -1)
             break;
-        
-        int flow = INF;
-        int nxt = E;
-        while (nxt != S) {
+
+        int f = INT_MAX;
+        int nxt = end;
+
+        while (nxt != start) {
             int cur = prv[nxt];
-            flow = min(flow, C[cur][nxt] - F[cur][nxt]);
+            f = min(f, capacity[cur][nxt] - flow[cur][nxt]);
             nxt = cur;
         }
-        ans += flow;
-        nxt = E;
-        while (nxt != S) {
+
+        ans += f;
+        nxt = end;
+
+        while (nxt != start) {
             int cur = prv[nxt];
-            F[cur][nxt] += flow;
-            F[nxt][cur] -= flow;
+            flow[cur][nxt] += f;
+            flow[nxt][cur] -= f;
             nxt = cur;
         }
     }
