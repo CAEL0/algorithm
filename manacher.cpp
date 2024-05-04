7e67c5058df83e13b7f5cc8a460284f84d6eddeb
Jongmin Kim <kjongmin26@gmail.com>
2024-05-04 16:42:55 +0900

    Update manacher.cpp

--- manacher.cpp
+++ manacher.cpp
@@ -1,47 +1,55 @@
-// BOJ BOJ 13275 가장 긴 팰린드롬 부분 문자열
+// BOJ 13275 가장 긴 팰린드롬 부분 문자열
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
 #define fi first
 #define se second
- 
+
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 200005;
-int N, A[MAX];
-string S;
-char C[MAX];
-void manacher() {
-    int r = -1, c = -1;
-    for (int i = 1; i < N; i++) {
-        if (r < i - 1 + A[i - 1]) {
-            r = i - 1 + A[i - 1];
+vector<int> manacher(string &s) {
+    int n = 2 * s.sz + 1;
+
+    vector<char> w(n);
+    for (int i = 0; i < n; i++) {
+        if (i & 1)
+            w[i] = s[i / 2];
+        else
+            w[i] = '#';
+    }
+
+    int r = -1;
+    int c = -1;
+    vector<int> ret(n);
+
+    for (int i = 1; i < n; i++) {
+        if (r < i - 1 + ret[i - 1]) {
+            r = i - 1 + ret[i - 1];
             c = i - 1;
         }
+
         if (r >= i)
-            A[i] = min(r - i, A[2 * c - i]);
-        
-        while (i - A[i] > 0 && i + A[i] < N - 1 && C[i - A[i] - 1] == C[i + A[i] + 1])
-            A[i]++;
+            ret[i] = min(r - i, ret[2 * c - i]);
+
+        while (i - ret[i] > 0 && i + ret[i] < n - 1 && w[i - ret[i] - 1] == w[i + ret[i] + 1])
+            ret[i]++;
     }
+
+    return ret;
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    
-    cin >> S;
-    N = 2 * S.length() + 1;
-    for (int i = 0; i < N; i++) {
-        if (i & 1)
-            C[i] = S[i / 2];
-        else
-            C[i] = '#';
-    }
-    manacher();
-    cout << *max_element(A, A + N);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+
+    vector<int> v = manacher(s);
+
+    cout << *max_element(v.begin(), v.end());
 }
