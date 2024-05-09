a403d0bd34db090612a4a6c6dca06d6cedfeb210
Jongmin Kim <kjongmin26@gmail.com>
2024-05-09 19:30:41 +0900

    Update miller_rabin.cpp

--- miller_rabin.cpp
+++ miller_rabin.cpp
@@ -83,3 +83,85 @@ int main() {
 
     cout << ans;
 }
+
+//--------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+ll ipow(ll base, ll exp, ll mod) {
+    ll ret = 1;
+    base %= mod;
+
+    while (exp) {
+        if (exp & 1)
+            ret = (__int128)ret * base % mod;
+
+        exp >>= 1;
+        base = (__int128)base * base % mod;
+    }
+
+    return ret;
+}
+
+bool miller_rabin(ll x) {
+    if (x == 1)
+        return false;
+
+    int s = 0;
+    ll d = x - 1;
+
+    while (!(d & 1)) {
+        s++;
+        d >>= 1;
+    }
+
+    for (ll a : {2, 3, 5, 7, 11, 13, 17, 19, 23}) {
+        if (a >= x - 1)
+            break;
+
+        ll b = ipow(a, d, x);
+        if (b == 1 || b == x - 1)
+            continue;
+
+        if (s == 1)
+            return false;
+
+        bool flag = true;
+        for (int i = 0; i < s - 1; i++) {
+            b = (__int128)b * b % x;
+
+            if (b == x - 1) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        ll x;
+        cin >> x;
+
+        cout << (miller_rabin(x) ? "Yes" : "No") << '\n';
+    }
+}
