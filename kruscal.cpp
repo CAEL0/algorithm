2cf980ec8d79e490e9ecbcb58d8f2faaf45e643a
Jongmin Kim <kjongmin26@gmail.com>
2024-05-09 22:46:32 +0900

    Update kruscal.cpp

--- kruscal.cpp
+++ kruscal.cpp
@@ -1,6 +1,5 @@
 // BOJ 1197 최소 스패닝 트리
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,53 +10,61 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int V, E, dsu[10005];
+struct DisjointSet {
+    int n;
+    vector<int> dsu;
 
-struct Edge {
-    int x, y, w;
-    bool operator<(const Edge &o) const {
-        return w < o.w;
+    DisjointSet(int _n) {
+        n = _n;
+        dsu.resize(n + 1);
     }
-} graph[100005];
 
-int find(int z) {
-    if (z != dsu[z])
-        dsu[z] = find(dsu[z]);
-    return dsu[z];
-}
-void uni(int x, int y) {
-    x = find(x);
-    y = find(y);
-    dsu[x] = y;
-}
-int kruskal() {
-    int ans = 0, cnt = 0;
-    for (int i = 0; i < E; i++) {
-        if (find(graph[i].x) != find(graph[i].y)) {
-            uni(graph[i].x, graph[i].y);
-            ans += graph[i].w;
-            cnt++;
-        }
-        if (cnt == V - 1)
-            return ans;
+    void init() {
+        for (int i = 1; i <= n; i++)
+            dsu[i] = i;
     }
-    return -1;
-}
+
+    int find(int z) {
+        if (z != dsu[z])
+            dsu[z] = find(dsu[z]);
+        return dsu[z];
+    }
+
+    void merge(int x, int y) { dsu[find(y)] = find(x); }
+
+    bool is_same(int x, int y) { return find(x) == find(y); }
+};
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> V >> E;
-    
-    int a, b, c;
-    for (int i = 0; i < E; i++) {
-        cin >> a >> b >> c;
-        graph[i] = Edge{a, b, c};
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<pii, ll>> edges(m);
+    for (int i = 0; i < m; i++)
+        cin >> edges[i].fi.fi >> edges[i].fi.se >> edges[i].se;
+
+    sort(edges.begin(), edges.end(), [](auto &p, auto &q) { return p.se < q.se; });
+
+    DisjointSet ds = DisjointSet(n);
+    ds.init();
+
+    ll ans = 0;
+    int cnt = 0;
+
+    for (auto &edge : edges) {
+        if (ds.is_same(edge.fi.fi, edge.fi.se))
+            continue;
+
+        ds.merge(edge.fi.fi, edge.fi.se);
+        ans += edge.se;
+
+        if (++cnt == n - 1)
+            break;
     }
-    sort(graph, graph + E);
-    for (int i = 1; i <= V; i++)
-        dsu[i] = i;
-    
-    cout << kruskal();
+
+    cout << ans;
 }
