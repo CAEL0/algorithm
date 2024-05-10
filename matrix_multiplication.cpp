4296c3c5728b86d1b1d73ee335a72eeb6ebedc83
Jongmin Kim <kjongmin26@gmail.com>
2024-05-10 17:34:57 +0900

    Update matrix_multiplication.cpp

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -1,6 +1,5 @@
 // BOJ 17401 일하는 세포
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -12,58 +11,81 @@ typedef long long ll;
 typedef pair<int, int> pii;
 
 const int MOD = 1e9 + 7;
-int T, N, D;
 
 struct Matrix {
-    int m[25][25] = {0};
-    
-    Matrix operator* (const Matrix o) const {
-        Matrix res;
-        for (int i = 0; i < 25; i++)
-            for (int j = 0; j < 25; j++)
-                for (int k = 0; k < 25; k++)
-                    res.m[i][j] = (res.m[i][j] + (ll)m[i][k] * o.m[k][j]) % MOD;
-        return res;
+    int n;
+    vector<vector<int>> v;
+
+    Matrix(int _n) {
+        n = _n;
+        v.resize(n);
+
+        for (int i = 0; i < n; i++)
+            v[i].resize(n);
     }
-} graph[105], MQ, MR, ans;
+
+    Matrix operator*(const Matrix &o) const {
+        Matrix ret(n);
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    ret.v[i][j] = (ret.v[i][j] + (ll)v[i][k] * o.v[k][j]) % MOD;
+
+        return ret;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> T >> N >> D;
-    for (int i = 0; i < T; i++) {
+    int t, n, d;
+    cin >> t >> n >> d;
+
+    vector<Matrix> graph(t, Matrix(n));
+
+    for (int i = 0; i < t; i++) {
         int m;
         cin >> m;
+
         while (m--) {
             int a, b, c;
             cin >> a >> b >> c;
-            graph[i].m[--a][--b] = c;
+
+            graph[i].v[--a][--b] = c;
         }
     }
-    int q = D / T;
-    int r = D % T;
-    for (int i = 0; i < N; i++)
-        MQ.m[i][i] = MR.m[i][i] = ans.m[i][i] = 1;
-    
-    for (int i = 0; i < T; i++) {
-        MQ = MQ * graph[i];
+
+    int q = d / t;
+    int r = d % t;
+
+    Matrix x(n), y(n), ans(n);
+
+    for (int i = 0; i < n; i++)
+        x.v[i][i] = y.v[i][i] = ans.v[i][i] = 1;
+
+    for (int i = 0; i < t; i++) {
+        x = x * graph[i];
         if (i < r)
-            MR = MR * graph[i];
+            y = y * graph[i];
     }
+
     while (q) {
         if (q & 1)
-            ans = ans * MQ;
-        
-        q >>= 1;
-        MQ = MQ * MQ;
-    }
-    ans = ans * MR;
+            ans = ans * x;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << ans.m[i][j] << ' ';
-        }
+        q >>= 1;
+        x = x * x;
+    }
+
+    ans = ans * y;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << ans.v[i][j] << ' ';
+
         cout << '\n';
     }
 }
