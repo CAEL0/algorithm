9dd604fbb22d09a9175d52129e2ca6171fd9ac4d
Jongmin Kim <kjongmin26@gmail.com>
2024-05-13 00:58:22 +0900

    Update range_update_point_query.cpp

--- range_update_point_query.cpp
+++ range_update_point_query.cpp
@@ -1,6 +1,5 @@
 // BOJ 16975 수열과 쿼리 21
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,60 +10,85 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 100005;
-int N, Q, A[MAX];
-ll tree[4 * MAX];
+struct SegmentTree {
+    vector<ll> tree;
 
-void range(int idx, int s, int e, int l, int r, ll v) {
-    if (r < s || e < l)
-        return;
-    
-    if (l <= s && e <= r) {
-        tree[idx] += v;
-        return;
+    SegmentTree(int n) { tree.resize(4 * n); }
+
+    void init(int idx, int s, int e, vector<ll> &v) {
+        if (s == e) {
+            tree[idx] = v[s];
+            return;
+        }
+
+        int m = (s + e) >> 1;
+        init(2 * idx, s, m, v);
+        init(2 * idx + 1, m + 1, e, v);
     }
-    int m = (s + e) >> 1;
-    range(2 * idx, s, m, l, r, v);
-    range(2 * idx + 1, m + 1, e, l, r, v);
-}
-ll point(int idx, int s, int e, int l) {
-    if (l < s || e < l)
-        return 0;
-    
-    if (s == e)
-        return tree[idx];
-    
-    int m = (s + e) >> 1;
-    return tree[idx] + point(2 * idx, s, m, l) + point(2 * idx + 1, m + 1, e, l);
-}
+
+    ll point(int idx, int s, int e, int l) {
+        if (l < s || e < l)
+            return 0;
+
+        if (s == e)
+            return tree[idx];
+
+        int m = (s + e) >> 1;
+        return tree[idx] + point(2 * idx, s, m, l) + point(2 * idx + 1, m + 1, e, l);
+    }
+
+    void range(int idx, int s, int e, int l, int r, ll x) {
+        if (r < s || e < l)
+            return;
+
+        if (l <= s && e <= r) {
+            tree[idx] += x;
+            return;
+        }
+        int m = (s + e) >> 1;
+        range(2 * idx, s, m, l, r, x);
+        range(2 * idx + 1, m + 1, e, l, r, x);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-        cin >> A[i];
-    
-    cin >> Q;
-    while (Q--) {
-        int q;
-        cin >> q;
-        if (q == 1) {
-            int i, j, k;
-            cin >> i >> j >> k;
-            range(1, 1, N, i, j, k);
+    int n;
+    cin >> n;
+
+    vector<ll> v(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
+
+    SegmentTree st = SegmentTree(n);
+    st.init(1, 1, n, v);
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int op;
+        cin >> op;
+
+        if (op == 1) {
+            ll l, r, x;
+            cin >> l >> r >> x;
+
+            st.range(1, 1, n, l, r, x);
         } else {
-            int x;
-            cin >> x;
-            cout << point(1, 1, N, x) + A[x] << '\n';
+            int l;
+            cin >> l;
+
+            cout << st.point(1, 1, n, l) << '\n';
         }
     }
 }
 
 //--------------------------------------------------------------------------------
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -107,7 +131,8 @@ struct Fenwick {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
     cin >> N;
     for (int i = 1; i <= N; i++) {
