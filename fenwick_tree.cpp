e7ffafe631578a2297279652e7ad837c50983c35
Jongmin Kim <kjongmin26@gmail.com>
2024-05-14 01:10:49 +0900

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -10,43 +10,50 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 1000005;
-
 struct FenwickTree {
     int n;
-    ll tree[MAX];
+    vector<ll> tree;
 
-    FenwickTree(int n) { this->n = n; }
+    FenwickTree(int _n) {
+        n = _n;
+        tree.resize(n + 1);
+    }
 
-    ll summation(int k) {
+    ll sum(int k) {
         ll ret = 0;
+
         while (k > 0) {
             ret += tree[k];
             k -= (k & -k);
         }
+
         return ret;
     }
 
-    ll summation(int l, int r) { return summation(r) - summation(l - 1); }
+    ll sum(int l, int r) { return sum(r) - sum(l - 1); }
 
     ll kth(ll k) {
-        ll ret = 0;
         ll idx = 1;
         while (idx <= n)
-            idx *= 2;
+            idx <<= 1;
+
+        ll ret = 0;
 
         while (idx) {
             if (ret + idx <= n && tree[ret + idx] < k) {
                 k -= tree[ret + idx];
                 ret += idx;
             }
-            idx /= 2;
+
+            idx >>= 1;
         }
+
         return ret + 1;
     }
 
     void update(int k, ll v) {
-        ll gap = v - summation(k, k);
+        ll gap = v - sum(k, k);
+
         while (k <= n) {
             tree[k] += gap;
             k += (k & -k);
@@ -69,20 +76,23 @@ int main() {
     int n, p, q;
     cin >> n >> p >> q;
 
-    FenwickTree ft = FenwickTree(n);
+    FenwickTree ft(n);
 
     for (int i = 1; i <= n; i++) {
         ll x;
         cin >> x;
+
         ft.update(i, x);
     }
+
     q += p;
     while (q--) {
         ll op, x, y;
         cin >> op >> x >> y;
+
         if (op == 1)
             ft.update(x, y);
         else
-            cout << ft.summation(x, y) << '\n';
+            cout << ft.sum(x, y) << '\n';
     }
 }
