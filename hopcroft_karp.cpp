c0cf498be3da65327a2c6ef62bbe7b122818bd6a
Jongmin Kim <kjongmin26@gmail.com>
2024-06-03 19:10:48 +0900

    Update hopcroft_karp.cpp

--- hopcroft_karp.cpp
+++ hopcroft_karp.cpp
@@ -1,6 +1,5 @@
 // BOJ 2188 축사 배정
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,145 +10,80 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 205;
-const int INF = 2e9;
-int N, M, A[MAX], B[MAX], lvl[MAX];
-bool used[MAX];
-vector<int> G[MAX];
-
-bool dfs(int cur) {
-    for (int nxt: G[cur]) {
-        if (B[nxt] == -1 || lvl[B[nxt]] == lvl[cur] + 1 && dfs(B[nxt])) {
-            used[cur] = true;
-            A[cur] = nxt;
-            B[nxt] = cur;
+bool dfs(int cur, vector<int> &a, vector<int> &b, vector<int> &lvl, vector<vector<int>> &graph) {
+    for (int nxt : graph[cur]) {
+        if (lvl[b[nxt]] == lvl[cur] + 1 && (b[nxt] == 0 || dfs(b[nxt], a, b, lvl, graph))) {
+            a[cur] = nxt;
+            b[nxt] = cur;
             return true;
         }
     }
+
+    lvl[cur] = INT_MAX;
     return false;
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        int s;
-        cin >> s;
-        while (s--) {
-            int tmp;
-            cin >> tmp;
-            G[i].push_back(tmp);
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> graph(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        int k;
+        cin >> k;
+
+        while (k--) {
+            int x;
+            cin >> x;
+
+            graph[i].push_back(x);
         }
     }
-    memset(A, -1, sizeof(A));
-    memset(B, -1, sizeof(B));
+
+    vector<int> a(n + 1);
+    vector<int> b(m + 1);
+    vector<int> lvl(n + 1);
     int ans = 0;
+
     while (1) {
-        queue<int> Q;
-        for (int i = 1; i <= N; i++) {
-            if (!used[i]) {
+        lvl[0] = INT_MAX;
+        deque<int> dq;
+
+        for (int i = 1; i <= n; i++) {
+            if (a[i] == 0) {
                 lvl[i] = 0;
-                Q.push(i);
+                dq.push_back(i);
             } else
-                lvl[i] = INF;
+                lvl[i] = INT_MAX;
         }
-        while (Q.sz) {
-            int cur = Q.front();
-            Q.pop();
-            for (int nxt: G[cur]) {
-                if (B[nxt] != -1 && lvl[B[nxt]] == INF) {
-                    lvl[B[nxt]] = lvl[cur] + 1;
-                    Q.push(B[nxt]);
+
+        while (dq.sz) {
+            int cur = dq.front();
+            dq.pop_front();
+
+            for (int nxt : graph[cur]) {
+                if (lvl[b[nxt]] == INT_MAX) {
+                    lvl[b[nxt]] = lvl[cur] + 1;
+                    dq.push_back(b[nxt]);
                 }
             }
         }
+
         int flow = 0;
-        for (int i = 1; i <= N; i++)
-            if (!used[i] && dfs(i))
+        for (int i = 1; i <= n; i++)
+            if (!a[i] && dfs(i, a, b, lvl, graph))
                 flow++;
-        
-        ans += flow;
-        if (!flow)
+
+        if (flow == 0)
             break;
-    }
-    cout << ans;
-}
 
-//--------------------------------------------------------------------------------
-
-#include <iostream>
-#include <bits/stdc++.h>
-#define sz size()
-#define bk back()
-#define fi first
-#define se second
-
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-
-const int MAX = 205;
-const int INF = 2e9;
-int N, M, A[MAX], B[MAX], lvl[MAX];
-vector<int> G[MAX];
-
-bool dfs(int cur) {
-    for (int nxt: G[cur]) {
-        if (lvl[B[nxt]] == lvl[cur] + 1 && (!B[nxt] || dfs(B[nxt]))) {
-            A[cur] = nxt;
-            B[nxt] = cur;
-            return true;
-        }
-    }
-    lvl[cur] = INF;
-    return false;
-}
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        int s;
-        cin >> s;
-        while (s--) {
-            int tmp;
-            cin >> tmp;
-            G[i].push_back(tmp);
-        }
-    }
-    int ans = 0;
-    while (1) {
-        queue<int> Q;
-        lvl[0] = INF;
-        for (int i = 1; i <= N; i++) {
-            if (!A[i]) {
-                lvl[i] = 0;
-                Q.push(i);
-            } else
-                lvl[i] = INF;
-        }
-        while (Q.sz) {
-            int cur = Q.front();
-            Q.pop();
-            for (int nxt: G[cur]) {
-                if (lvl[B[nxt]] == INF) {
-                    lvl[B[nxt]] = lvl[cur] + 1;
-                    Q.push(B[nxt]);
-                }
-            }
-        }
-        int flow = 0;
-        for (int i = 1; i <= N; i++)
-            if (!A[i] && dfs(i))
-                flow++;
-        
         ans += flow;
-        if (!flow)
-            break;
     }
+
     cout << ans;
 }
