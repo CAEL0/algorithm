1f233a518f86d12a03424fbac7be12d6df1803e4
Jongmin Kim <kjongmin26@gmail.com>
2024-06-06 11:39:28 +0900

    Update extended_euclidean.cpp

--- extended_euclidean.cpp
+++ extended_euclidean.cpp
@@ -1,6 +1,5 @@
 // BOJ 14565 역원(Inverse) 구하기
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -10,16 +9,11 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
-ll gcd(ll x, ll y) {
-    while (x % y) {
-        swap(x, y);
-        y %= x;
-    }
-    return y;
-}
 pll extended_gcd(ll x, ll y) {
-    ll a0 = 1, a1 = 0, b0 = 0, b1 = 1, k;
+    ll a0 = 1, a1 = 0, b0 = 0, b1 = 1, k, yy = y;
+    
     while (y) {
         k = x / y;
         swap(x, y);
@@ -29,24 +23,23 @@ pll extended_gcd(ll x, ll y) {
         swap(b0, b1);
         b1 -= k * b0;
     }
+    
+    if (a0 < 0)
+        a0 += yy;
+    
     return pll(a0, b0);
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
     ll n, a;
     cin >> n >> a;
-    
+
     if (gcd(n, a) != 1)
         cout << n - a << ' ' << -1;
-    else {
-        ll c, d;
-        tie(c, d) = extended_gcd(n, a);
-        if (d < 0)
-            cout << n - a << ' ' << n + d;
-        else
-            cout << n - a << ' ' << d;
-    }
+    else
+        cout << n - a << ' ' << extended_gcd(a, n).fi;
 }
