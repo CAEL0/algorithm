9fc07010432e0f8a5de8162acf5cc16d82d41b47
Jongmin Kim <kjongmin26@gmail.com>
2024-06-07 15:15:48 +0900

    Update bellman_ford.cpp

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -1,6 +1,5 @@
-# BOJ 11657 타임머신
+// BOJ 11657 타임머신
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -9,39 +8,44 @@
 
 using namespace std;
 typedef long long ll;
-typedef pair<ll, ll> pii;
-
-const int MAX = 505;
-const ll INF = 1e18;
-ll V, E, D[MAX];
-vector<pii> G[MAX];
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> V >> E;
-    for (int i = 0; i < E; i++) {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pll>> graph(n + 1);
+
+    while (m--) {
         int u, v, w;
         cin >> u >> v >> w;
-        G[u].push_back(pii(v, w));
+
+        graph[u].push_back({v, w});
     }
-    fill_n(D, MAX, INF);
-    D[1] = 0;
-    for (int i = 0; i < V - 1; i++)
-        for (int u = 1; u <= V; u++)
-            for (pii p: G[u])
-                if (D[u] != INF)
-                    D[p.fi] = min(D[p.fi], D[u] + p.se);
-    
-    for (int u = 1; u <= V; u++) {
-        for (pii p: G[u]) {
-            if (D[u] != INF && D[p.fi] > D[u] + p.se) {
+
+    vector<ll> dist(n + 1, LLONG_MAX);
+    dist[1] = 0;
+
+    for (int i = 1; i < n; i++)
+        for (int cur = 1; cur <= n; cur++)
+            for (pll &nxt : graph[cur])
+                if (dist[cur] != LLONG_MAX)
+                    dist[nxt.fi] = min(dist[nxt.fi], dist[cur] + nxt.se);
+
+    for (int cur = 1; cur <= n; cur++) {
+        for (pll &nxt : graph[cur]) {
+            if (dist[cur] != LLONG_MAX && dist[nxt.fi] > dist[cur] + nxt.se) {
                 cout << -1;
                 return 0;
             }
         }
     }
-    for (int u = 2; u <= V; u++)
-        cout << (D[u] == INF ? -1 : D[u]) << '\n';
+
+    for (int i = 2; i <= n; i++)
+        cout << (dist[i] == LLONG_MAX ? -1 : dist[i]) << '\n';
 }
