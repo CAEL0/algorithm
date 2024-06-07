d23df8a9571218bce641c3d79e4c0f4449a20128
Jongmin Kim <kjongmin26@gmail.com>
2024-06-07 15:19:47 +0900

    Update floyd_warshall.cpp

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -1,6 +1,5 @@
 // BOJ 11404 플로이드
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,32 +10,35 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 105;
-int V, E, G[MAX][MAX];
-
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> V >> E;
-    for (int i = 1; i <= V; i++)
-        for (int j = 1; j <= V; j++)
-            if (i != j)
-                G[i][j] = 1e9;
-    
-    while (E--) {
-        int i, j, w;
-        cin >> i >> j >> w;
-        G[i][j] = min(G[i][j], w);
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<ll>> graph(n + 1, vector<ll>(n + 1, 1e18));
+
+    for (int i = 1; i <= n; i++)
+        graph[i][i] = 0;
+
+    while (m--) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+
+        graph[u][v] = min(graph[u][v], w);
     }
-    for (int k = 1; k <= V; k++)
-        for (int i = 1; i <= V; i++)
-            for (int j = 1; j <= V; j++)
-                G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
-    
-    for (int i = 1; i <= V; i++) {
-        for (int j = 1; j <= V; j++)
-            cout << (G[i][j] == 1e9 ? 0 : G[i][j]) << ' ';
+
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++)
+            cout << (graph[i][j] == 1e18 ? 0 : graph[i][j]) << ' ';
+
         cout << '\n';
     }
 }
