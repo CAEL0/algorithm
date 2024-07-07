e9cbd0e239d1888412bb7367096e0e8eed686146
Jongmin Kim <kjongmin26@gmail.com>
2024-07-07 13:34:48 +0900

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -13,28 +12,35 @@ struct Node {
     int idx[26];
     bool finish;
 };
+
 struct Trie {
     vector<Node> nodes{Node()};
 
     void insert(const string s) {
         int cur = 0;
-        for (char c: s) {
+
+        for (char c : s) {
             if (!nodes[cur].idx[c - 97]) {
                 nodes[cur].idx[c - 97] = nodes.size();
                 nodes.push_back(Node());
             }
+
             cur = nodes[cur].idx[c - 97];
         }
+
         nodes[cur].finish = true;
     }
+
     bool search(const string s) {
         int cur = 0;
-        for (char c: s) {
-            if (!nodes[cur].idx[c - 97]) {
+
+        for (char c : s) {
+            if (!nodes[cur].idx[c - 97])
                 return false;
-            }
+
             cur = nodes[cur].idx[c - 97];
         }
+
         return nodes[cur].finish;
     }
 };
@@ -42,26 +48,25 @@ struct Trie {
 //--------------------------------------------------------------------------------
 
 struct Trie {
-    map<char, Trie*> nxt;
+    map<char, Trie *> nxt;
     bool finish;
 
-    Trie() {
-        finish = false;
-    }
-    ~Trie() {
-        nxt.clear();
-    }
-    bool insert(const char* s) {
+    Trie() { finish = false; }
+
+    ~Trie() { nxt.clear(); }
+
+    bool insert(const char *s) {
         if (*s == '\0') {
             finish = true;
             return !nxt.sz;
         }
+
         if (finish)
             return false;
-        
+
         if (nxt.find(*s) == nxt.end())
             nxt[*s] = new Trie;
-        
+
         return nxt[*s]->insert(s + 1);
     }
 };
@@ -69,26 +74,26 @@ struct Trie {
 //--------------------------------------------------------------------------------
 
 struct Trie {
-    vector<pair<char, Trie*>> nxt;
+    vector<pair<char, Trie *>> nxt;
     bool finish;
 
-    Trie() {
-        finish = false;
-    }
-    ~Trie() {
-        nxt.clear();
-    }
-    void insert(const char* s) {
+    Trie() { finish = false; }
+
+    ~Trie() { nxt.clear(); }
+
+    void insert(const char *s) {
         if (*s == '\0') {
             finish = true;
             return;
         }
-        for (auto node: nxt) {
+
+        for (auto node : nxt) {
             if (node.fi == *s) {
                 node.se->insert(s + 1);
                 return;
             }
         }
+
         nxt.push_back({*s, new Trie});
         nxt.bk.se->insert(s + 1);
     }
