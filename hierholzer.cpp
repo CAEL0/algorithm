d26c14b86472cee6bcc9c8ddfe2e0ae50d4ad4f9
Jongmin Kim <kjongmin26@gmail.com>
2024-07-22 16:26:08 +0900

    Update hierholzer.cpp

--- hierholzer.cpp
+++ hierholzer.cpp
@@ -10,6 +10,73 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+void dfs(int cur, vector<int> &edges, vector<vector<int>> &graph, vector<bool> &used) {
+    while (graph[cur].sz) {
+        int idx = graph[cur].bk;
+        graph[cur].pop_back();
+
+        if (used[idx])
+            continue;
+
+        used[idx] = true;
+        int nxt = edges[idx] ^ cur;
+
+        dfs(nxt, edges, graph, used);
+    }
+
+    cout << cur << ' ';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> edges;
+    vector<vector<int>> graph(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int x;
+            cin >> x;
+
+            if (i > j)
+                continue;
+
+            while (x--) {
+                graph[i].push_back(edges.sz);
+                graph[j].push_back(edges.sz);
+                edges.push_back(i ^ j);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (graph[i].sz & 1) {
+            cout << -1;
+            return 0;
+        }
+    }
+
+    vector<bool> used(edges.sz);
+    dfs(1, edges, graph, used);
+}
+
+//--------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
 void dfs(int cur, vector<vector<int>> &graph) {
     for (int nxt = 1; nxt < graph.sz; nxt++) {
         int k = 0;
