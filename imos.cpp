da26f3d75430452d6a409cae8f8c1886b29fa185
Jongmin Kim <kjongmin26@gmail.com>
2024-07-28 17:02:19 +0900

    Update and rename imos_method.cpp to imos.cpp

--- imos.cpp
+++ imos.cpp
@@ -1,6 +1,5 @@
 // BOJ 5541 釘 (Nails)
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,15 +10,16 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 5005;
-int N, M, imos[MAX][MAX];
-
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M;
-    while (M--) {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> imos(n + 5, vector<int>(n + 5));
+    while (m--) {
         int a, b, x;
         cin >> a >> b >> x;
 
@@ -30,24 +30,27 @@ int main() {
         imos[a + x + 1][b + x + 2]++;
         imos[a + x + 2][b + x + 2]--;
     }
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
             imos[i][j] += imos[i][j - 1];
-    
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
             imos[i][j] += imos[i - 1][j];
 
-    for (int k = 2; k <= 2 * N; k++) {
-        for (int i = max(1, k - N); i <= min(N, k - 1); i++) {
+    for (int k = 2; k <= 2 * n; k++) {
+        for (int i = max(1, k - n); i <= min(n, k - 1); i++) {
             int j = k - i;
             imos[i][j] += imos[i - 1][j - 1];
         }
     }
+
     int ans = 0;
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
-            ans += (imos[i][j] > 0);
-    
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            if (imos[i][j] > 0)
+                ans++;
+
     cout << ans;
 }
