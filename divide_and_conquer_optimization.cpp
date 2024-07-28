d3558a3531417bc8531aa1cdaa10937be345c874
Jongmin Kim <kjongmin26@gmail.com>
2024-07-28 15:46:52 +0900

    Update divide_and_conquer_optimization.cpp

--- divide_and_conquer_optimization.cpp
+++ divide_and_conquer_optimization.cpp
@@ -1,34 +1,31 @@
 // BOJ 11001 김치
 
 #include <bits/stdc++.h>
-#define fi first
-#define se second
 #define sz size()
 #define bk back()
+#define fi first
+#define se second
 
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 100005;
-int n, d, t[MAX], v[MAX];
-ll ans;
+ll f(int i, int j, vector<ll> &t, vector<ll> &v) { return t[j] * (j - i) + v[i]; }
 
-ll f(int i, int j) { return (ll)t[j] * (j - i) + v[i]; }
-
-void g(int s, int e, int l, int r) {
+void g(int s, int e, int l, int r, vector<ll> &t, vector<ll> &v, int d, ll &ans) {
     if (s > e)
         return;
 
     int i = (s + e) >> 1;
     int k = i;
     for (int j = max(i + 1, l); j <= min(i + d, r); j++)
-        if (f(i, k) < f(i, j))
+        if (f(i, k, t, v) < f(i, j, t, v))
             k = j;
 
-    ans = max(ans, f(i, k));
-    g(s, i - 1, l, k);
-    g(i + 1, e, k, r);
+    ans = max(ans, f(i, k, t, v));
+
+    g(s, i - 1, l, k, t, v, d, ans);
+    g(i + 1, e, k, r, t, v, d, ans);
 }
 
 int main() {
@@ -36,13 +33,20 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
+    int n, d;
     cin >> n >> d;
+
+    vector<ll> t(n + 1);
     for (int i = 1; i <= n; i++)
         cin >> t[i];
 
+    vector<ll> v(n + 1);
     for (int i = 1; i <= n; i++)
         cin >> v[i];
 
-    g(1, n, 1, n);
+    ll ans = 0;
+
+    g(1, n, 1, n, t, v, d, ans);
+
     cout << ans;
 }
