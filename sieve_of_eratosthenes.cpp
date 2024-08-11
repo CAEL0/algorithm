66b47254cececf6c666d337e89efd71e0e630917
Jongmin Kim <kjongmin26@gmail.com>
2024-08-11 17:37:30 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -10,18 +10,24 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+vector<bool> get_sieve(int n) {
+    vector<bool> sieve(n, true);
+    sieve[0] = sieve[1] = false;
+
+    for (int i = 2; i < n; i++)
+        if (sieve[i])
+            for (ll j = (ll)i * i; j < n; j += i)
+                sieve[j] = false;
+    
+    return sieve;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    vector<bool> sieve(10000005, true);
-    sieve[0] = sieve[1] = false;
-
-    for (int i = 2; i < sieve.sz; i++)
-        if (sieve[i])
-            for (ll j = (ll)i * i; j < sieve.sz; j += i)
-                sieve[j] = false;
+    vector<bool> sieve = get_sieve(10000005);
 
     int k;
     cin >> k;
