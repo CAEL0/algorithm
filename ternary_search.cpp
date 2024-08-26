5467c8a7589af7431d6b11f8b6ecfa770497b959
Jongmin Kim <kjongmin26@gmail.com>
2024-08-26 15:17:06 +0900

    Update ternary_search.cpp

--- ternary_search.cpp
+++ ternary_search.cpp
@@ -1,6 +1,5 @@
 // BOJ 8986 전봇대
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,40 +10,46 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int N, X[100005];
-
-ll f(int k) {
+ll f(ll k, vector<int> &v) {
     ll ret = 0;
     ll x = k;
-    for (int i = 1; i < N; i++) {
-        ret += abs(X[i] - x);
+
+    for (int i = 1; i < v.sz; i++) {
+        ret += abs(v[i] - x);
         x += k;
     }
+
     return ret;
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N;
-    for (int i = 0; i < N; i++)
-        cin >> X[i];
-    
-    int left = 0;
-    int right = X[N - 1];
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    ll left = 0;
+    ll right = v[n - 1];
+
     while (left + 3 <= right) {
-        int p = ((ll)2 * left + right) / 3;
-        int q = ((ll)left + 2 * right) / 3;
-        
-        if (f(p) >= f(q))
+        ll p = ((ll)2 * left + right) / 3;
+        ll q = ((ll)left + 2 * right) / 3;
+
+        if (f(p, v) >= f(q, v))
             left = p;
         else
             right = q;
     }
-    ll ans = f(left);
+
+    ll ans = f(left, v);
     for (int x = left + 1; x <= right; x++)
-        ans = min(ans, f(x));
-    
+        ans = min(ans, f(x, v));
+
     cout << ans;
 }
