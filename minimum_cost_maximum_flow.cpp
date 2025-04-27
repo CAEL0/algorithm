0f47b06d067953bfe729e39fe7a55a9abf04fdc9
Jongmin Kim <kjongmin26@gmail.com>
2025-04-27 14:40:25 +0900

    Update minimum_cost_maximum_flow.cpp

--- minimum_cost_maximum_flow.cpp
+++ minimum_cost_maximum_flow.cpp
@@ -1,6 +1,5 @@
 // BOJ 11407 책 구매하기 3
 
-#include <iostream>
 #include <bits/stdc++.h>
 #define sz size()
 #define bk back()
@@ -11,88 +10,107 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int MAX = 205;
-const int INF = 2e9;
-const int S = MAX - 2;
-const int E = MAX - 1;
-int N, M, C[MAX][MAX], D[MAX][MAX], F[MAX][MAX];
-vector<int> G[MAX];
-
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        cin >> C[S][i];
-        G[S].push_back(i);
-        G[i].push_back(S);
+    int n, m;
+    cin >> n >> m;
+
+    int mx = n + m + 3;
+    int source = mx - 2;
+    int sink = mx - 1;
+    vector<vector<int>> capacity(mx, vector<int>(mx));
+    vector<vector<int>> cost(mx, vector<int>(mx));
+    vector<vector<int>> flow(mx, vector<int>(mx));
+    vector<vector<int>> graph(mx);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> capacity[source][i];
+
+        graph[source].push_back(i);
+        graph[i].push_back(source);
     }
-    for (int j = N + 1; j <= N + M; j++) {
-        cin >> C[j][E];
-        G[j].push_back(E);
-        G[E].push_back(j);
+
+    for (int j = n + 1; j <= n + m; j++) {
+        cin >> capacity[j][sink];
+
+        graph[j].push_back(sink);
+        graph[sink].push_back(j);
     }
-    for (int j = N + 1; j <= N + M; j++) {
-        for (int i = 1; i <= N; i++) {
-            cin >> C[i][j];
-            G[i].push_back(j);
-            G[j].push_back(i);
+
+    for (int j = n + 1; j <= n + m; j++) {
+        for (int i = 1; i <= n; i++) {
+            cin >> capacity[i][j];
+
+            graph[i].push_back(j);
+            graph[j].push_back(i);
         }
     }
-    for (int j = N + 1; j <= N + M; j++) {
-        for (int i = 1; i <= N; i++) {
-            cin >> D[i][j];
-            D[j][i] = -D[i][j];
+
+    for (int j = n + 1; j <= n + m; j++) {
+        for (int i = 1; i <= n; i++) {
+            cin >> cost[i][j];
+
+            cost[j][i] = -cost[i][j];
         }
     }
-    int ans1 = 0;
-    int ans2 = 0;
+
+    int max_flow = 0;
+    int min_cost = 0;
+    vector<int> prv(mx);
+    vector<int> dist(mx);
+    vector<bool> in_dq(mx);
     while (1) {
-        int res[MAX], prv[MAX];
-        bool isin[MAX];
-        fill_n(res, MAX, INF);
-        memset(prv, -1, sizeof(prv));
-        memset(isin, false, sizeof(isin));
-        res[S] = 0;
-        isin[S] = true;
-        queue<int> Q({S});
-        while (Q.sz) {
-            int cur = Q.front();
-            Q.pop();
-            isin[cur] = false;
-            for (int nxt: G[cur]) {
-                if (C[cur][nxt] > F[cur][nxt] && res[nxt] > res[cur] + D[cur][nxt]) {
-                    res[nxt] = res[cur] + D[cur][nxt];
+        fill(prv.begin(), prv.end(), -1);
+        fill(dist.begin(), dist.end(), INT_MAX);
+        fill(in_dq.begin(), in_dq.end(), false);
+
+        deque<int> dq = {source};
+        dist[source] = 0;
+        in_dq[source] = true;
+
+        while (dq.sz) {
+            int cur = dq.front();
+            dq.pop_front();
+
+            in_dq[cur] = false;
+            for (int nxt : graph[cur]) {
+                if (capacity[cur][nxt] > flow[cur][nxt] && dist[nxt] > dist[cur] + cost[cur][nxt]) {
+                    dist[nxt] = dist[cur] + cost[cur][nxt];
                     prv[nxt] = cur;
-                    if (!isin[nxt]) {
-                        isin[nxt] = true;
-                        Q.push(nxt);
+                    if (!in_dq[nxt]) {
+                        in_dq[nxt] = true;
+                        dq.push_back(nxt);
                     }
                 }
             }
         }
-        if (prv[E] == -1)
+
+        if (prv[sink] == -1)
             break;
-        
-        int flow = INF;
-        int nxt = E;
-        while (nxt != S) {
+
+        int f = INT_MAX;
+        int nxt = sink;
+        while (nxt != source) {
             int cur = prv[nxt];
-            flow = min(flow, C[cur][nxt] - F[cur][nxt]);
+            f = min(f, capacity[cur][nxt] - flow[cur][nxt]);
             nxt = cur;
         }
-        nxt = E;
-        while (nxt != S) {
+
+        nxt = sink;
+        while (nxt != source) {
             int cur = prv[nxt];
-            ans2 += flow * D[cur][nxt];
-            F[cur][nxt] += flow;
-            F[nxt][cur] -= flow;
+            min_cost += f * cost[cur][nxt];
+            flow[cur][nxt] += f;
+            flow[nxt][cur] -= f;
             nxt = cur;
         }
     }
-    for (int i = 1; i <= N; i++)
-        ans1 += F[S][i];
-    
-    cout << ans1 << '\n' << ans2;
+
+    for (int i = 1; i <= n; i++)
+        max_flow += flow[source][i];
+
+    cout << max_flow << '\n' << min_cost;
 }
