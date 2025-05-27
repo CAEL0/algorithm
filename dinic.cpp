c6b7a0ba6f2d165909befb4f273308bc4c8b8be4
Jongmin Kim <kjongmin26@gmail.com>
2025-05-28 00:36:09 +0900

    Update dinic.cpp

--- dinic.cpp
+++ dinic.cpp
@@ -10,6 +10,153 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+void add_edge(int from, int to, int capacity, vector<vector<pair<pii, int>>> &graph) {
+    int from_idx = graph[from].sz;
+    int to_idx = graph[to].sz;
+
+    graph[from].push_back(make_pair(make_pair(to, to_idx), capacity));
+    graph[to].push_back(make_pair(make_pair(from, from_idx), 0));
+}
+
+int dfs(int cur, int flow, vector<vector<pair<pii, int>>> &graph, vector<int> &level, vector<int> &work, int sink) {
+    if (cur == sink)
+        return flow;
+
+    int ret = 0;
+    int level_cur = level[cur];
+    work[cur]--;
+    while (++work[cur] < graph[cur].sz) {
+        pair<pii, int> nxt = graph[cur][work[cur]];
+        if (level_cur <= level[nxt.fi.fi] || graph[nxt.fi.fi][nxt.fi.se].se == 0)
+            continue;
+
+        int k = dfs(nxt.fi.fi, min(flow - ret, graph[nxt.fi.fi][nxt.fi.se].se), graph, level, work, sink);
+        if (k <= 0)
+            continue;
+
+        graph[cur][work[cur]].se += k;
+        graph[nxt.fi.fi][nxt.fi.se].se -= k;
+        ret += k;
+
+        if (ret == flow)
+            return ret;
+    }
+
+    level[cur] = graph.sz;
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
+
+    int source = n + 1;
+    int sink = n + 2;
+    vector<vector<pair<pii, int>>> graph(n + 3);
+    for (int i = 1; i <= n; i++) {
+        if (v[i] == 1)
+            add_edge(source, i, INT_MAX, graph);
+        else if (v[i] == 2)
+            add_edge(i, sink, INT_MAX, graph);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int k;
+            cin >> k;
+
+            if (k > 0)
+                add_edge(i, j, k, graph);
+        }
+    }
+
+    int ans = 0;
+    vector<int> level(n + 3);
+    vector<int> work(n + 3);
+    deque<int> dq;
+    while (1) {
+        fill(level.begin(), level.end(), -1);
+        level[source] = 0;
+        dq = {source};
+        while (dq.sz) {
+            int cur = dq.front();
+            dq.pop_front();
+
+            for (auto nxt : graph[cur]) {
+                if (nxt.se == 0 || level[nxt.fi.fi] >= 0)
+                    continue;
+
+                level[nxt.fi.fi] = level[cur] + 1;
+                if (nxt.fi.fi == sink)
+                    break;
+
+                dq.push_back(nxt.fi.fi);
+            }
+
+            if (level[sink] != -1)
+                break;
+        }
+
+        if (level[sink] == -1)
+            break;
+
+        fill(work.begin(), work.end(), 0);
+        while (1) {
+            int flow = dfs(sink, INT_MAX, graph, level, work, source);
+            ans += flow;
+            if (flow == 0)
+                break;
+        }
+    }
+
+    dq = {source};
+    vector<bool> vst(n + 3);
+    while (dq.sz) {
+        int cur = dq.front();
+        dq.pop_front();
+
+        for (int i = 0; i < graph[cur].sz; i++) {
+            pair<pii, int> nxt = graph[cur][i];
+            if (graph[cur][i].se && !vst[nxt.fi.fi]) {
+                vst[nxt.fi.fi] = true;
+                dq.push_back(nxt.fi.fi);
+            }
+        }
+    }
+
+    cout << ans << '\n';
+
+    for (int i = 1; i <= n; i++)
+        if (vst[i])
+            cout << i << ' ';
+    cout << '\n';
+
+    for (int i = 1; i <= n; i++)
+        if (!vst[i])
+            cout << i << ' ';
+    cout << '\n';
+}
+
+//--------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+#define sz size()
+#define bk back()
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
 int dfs(int cur, int f, vector<vector<int>> &graph, vector<vector<int>> &capacity, vector<vector<int>> &flow, vector<int> &level, vector<int> &work, int sink) {
     if (cur == sink)
         return f;
