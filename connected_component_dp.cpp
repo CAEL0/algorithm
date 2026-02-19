273f5a417cdb8774646ac3f2b3207a17181caefb
Jongmin Kim <kjongmin26@gmail.com>
2026-02-20 00:46:25 +0900

    Update connected_component_dp.cpp

--- connected_component_dp.cpp
+++ connected_component_dp.cpp
@@ -23,25 +23,25 @@ int main() {
     vector<vector<ll>> dp(n + 1, vector<ll>(n + 1));
     dp[0][0] = 1;
     int k = 0;
-    for (int i = 1; i <= n; i++) {
-        if (i == s) {
+    for (int i = 0; i < n; i++) {
+        if (i + 1 == s) {
             k++;
-            for (int j = 0; j <= i - 1; j++) {
-                dp[i][j + 1] = (dp[i][j + 1] + dp[i - 1][j]) % MOD;
-                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+            for (int j = 0; j <= i; j++) {
+                dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j]) % MOD;
+                dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD;
             }
-        } else if (i == e) {
+        } else if (i + 1 == e) {
             k++;
-            for (int j = 0; j <= i - 1; j++) {
-                dp[i][j + 1] = (dp[i][j + 1] + dp[i - 1][j]) % MOD;
-                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+            for (int j = 0; j <= i; j++) {
+                dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j]) % MOD;
+                dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD;
             }
         } else {
-            for (int j = 0; j <= i - 1; j++)
-                dp[i][j + 1] = (dp[i][j + 1] + dp[i - 1][j] * (j + 1 - k) % MOD) % MOD;
+            for (int j = 0; j <= i; j++)
+                dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j] * (j + 1 - k) % MOD) % MOD;
 
-            for (int j = 2; j <= i - 1; j++)
-                dp[i][j - 1] = (dp[i][j - 1] + dp[i - 1][j] * (j - 1) % MOD) % MOD;
+            for (int j = 2; j <= i; j++)
+                dp[i + 1][j - 1] = (dp[i + 1][j - 1] + dp[i][j] * (j - 1) % MOD) % MOD;
         }
     }
 
