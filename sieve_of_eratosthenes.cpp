ae670961b57588414425da26970ce56608430e0c
Jongmin Kim <kjongmin26@gmail.com>
2026-03-06 11:06:40 +0900

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -42,3 +42,17 @@ int main() {
         }
     }
 }
+
+//--------------------------------------------------------------------------------
+
+vector<int> get_sieve(int n) {
+    vector<int> sieve(n + 1, -1);
+ 
+    for (int i = 2; i <= n; i++)
+        if (sieve[i] == -1)
+            for (ll j = (ll)i * i; j <= n; j += i)
+                if (sieve[j] == -1)
+                    sieve[j] = i;
+ 
+    return sieve;
+}
