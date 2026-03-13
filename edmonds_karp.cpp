5f1f1855f8be5a9c14208009e363412e791dc1bb
Jongmin Kim <kjongmin26@gmail.com>
2026-03-13 14:54:25 +0900

    Update edmonds_karp.cpp

--- edmonds_karp.cpp
+++ edmonds_karp.cpp
@@ -10,6 +10,73 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+struct EdmondsKarp {
+    int n, source, sink;
+    vector<vector<int>> graph, capacity, flow;
+
+    EdmondsKarp(int n) : n(n) {
+        source = n + 1;
+        sink = n + 2;
+        graph.resize(n + 3);
+        capacity.resize(n + 3, vector<int>(n + 3));
+        flow.resize(n + 3, vector<int>(n + 3));
+    }
+
+    void add_from_source(int to, int cap) { add_edge(source, to, cap); }
+
+    void add_to_sink(int from, int cap) { add_edge(from, sink, cap); }
+
+    void add_edge(int from, int to, int cap) {
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+        capacity[from][to] = cap;
+    }
+
+    int maximum_flow() {
+        int ret = 0;
+        while (1) {
+            vector<int> prv(n + 3, -1);
+            deque<int> dq = {source};
+            while (dq.sz && prv[sink] == -1) {
+                int cur = dq.front();
+                dq.pop_front();
+
+                for (int nxt : graph[cur]) {
+                    if (flow[cur][nxt] < capacity[cur][nxt] && prv[nxt] == -1) {
+                        prv[nxt] = cur;
+                        dq.push_back(nxt);
+
+                        if (nxt == sink)
+                            break;
+                    }
+                }
+            }
+
+            if (prv[sink] == -1)
+                break;
+
+            int f = INT_MAX;
+            int nxt = sink;
+            while (nxt != source) {
+                int cur = prv[nxt];
+                f = min(f, capacity[cur][nxt] - flow[cur][nxt]);
+                nxt = cur;
+            }
+
+            ret += f;
+            nxt = sink;
+            while (nxt != source) {
+                int cur = prv[nxt];
+                flow[cur][nxt] += f;
+                flow[nxt][cur] -= f;
+                nxt = cur;
+            }
+        }
+
+        return ret;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -18,80 +85,23 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int mx = n + m + 3;
-    int start = mx - 2;
-    int end = mx - 1;
-
-    vector<vector<int>> graph(mx);
-    vector<vector<int>> capacity(mx, vector<int>(mx, 0));
-    vector<vector<int>> flow(mx, vector<int>(mx, 0));
-
+    EdmondsKarp ek(n + m);
     for (int i = 1; i <= n; i++) {
-        graph[start].push_back(i);
-        graph[i].push_back(start);
-        capacity[start][i] = 1;
+        ek.add_from_source(i, 1);
 
-        int tmp;
-        cin >> tmp;
+        int k;
+        cin >> k;
 
-        while (tmp--) {
+        while (k--) {
             int j;
             cin >> j;
 
-            graph[i].push_back(n + j);
-            graph[n + j].push_back(i);
-            capacity[i][n + j] = 1;
+            ek.add_edge(i, n + j, 1);
         }
     }
 
-    for (int j = n + 1; j <= n + m; j++) {
-        graph[j].push_back(end);
-        graph[end].push_back(j);
-        capacity[j][end] = 1;
-    }
+    for (int j = 1; j <= m; j++)
+        ek.add_to_sink(n + j, 1);
 
-    int ans = 0;
-
-    while (1) {
-        vector<int> prv(mx, -1);
-        deque<int> dq({start});
-
-        while (dq.sz && prv[end] == -1) {
-            int cur = dq.front();
-            dq.pop_front();
-
-            for (int nxt : graph[cur]) {
-                if (capacity[cur][nxt] > flow[cur][nxt] && prv[nxt] == -1) {
-                    prv[nxt] = cur;
-                    dq.push_back(nxt);
-
-                    if (nxt == end)
-                        break;
-                }
-            }
-        }
-
-        if (prv[end] == -1)
-            break;
-
-        int f = INT_MAX;
-        int nxt = end;
-
-        while (nxt != start) {
-            int cur = prv[nxt];
-            f = min(f, capacity[cur][nxt] - flow[cur][nxt]);
-            nxt = cur;
-        }
-
-        ans += f;
-        nxt = end;
-
-        while (nxt != start) {
-            int cur = prv[nxt];
-            flow[cur][nxt] += f;
-            flow[nxt][cur] -= f;
-            nxt = cur;
-        }
-    }
-    cout << ans;
+    cout << ek.maximum_flow();
 }
