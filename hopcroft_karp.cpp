cf8150b400900916287c1316c0503a84471c266b
Jongmin Kim <kjongmin26@gmail.com>
2026-03-18 12:57:33 +0900

    Update hopcroft_karp.cpp

--- hopcroft_karp.cpp
+++ hopcroft_karp.cpp
@@ -1,4 +1,4 @@
-// BOJ 11375 열혈강호
+// BOJ 2051 최소 버텍스 커버
 
 #include <bits/stdc++.h>
 #define sz size()
@@ -75,6 +75,19 @@ struct HopcroftKarp {
         lvl[cur] = INT_MAX;
         return false;
     }
+
+    void minimum_vertex_cover(int cur, vector<bool> &left, vector<bool> &right) {
+        for (int nxt : graph[cur]) {
+            if (a[cur] == nxt || right[nxt])
+                continue;
+
+            right[nxt] = true;
+            if (!left[b[nxt]]) {
+                left[b[nxt]] = true;
+                minimum_vertex_cover(b[nxt], left, right);
+            }
+        }
+    }
 };
 
 int main() {
@@ -98,5 +111,34 @@ int main() {
         }
     }
 
-    cout << hk.maximum_matching();
+    cout << hk.maximum_matching() << '\n';
+
+    vector<bool> left(n + 1);
+    vector<bool> right(m + 1);
+    for (int i = 1; i <= n; i++) {
+        if (hk.a[i] == 0 && !left[i]) {
+            left[i] = true;
+            hk.minimum_vertex_cover(i, left, right);
+        }
+    }
+
+    vector<int> left_ans;
+    for (int i = 1; i <= n; i++)
+        if (!left[i])
+            left_ans.push_back(i);
+
+    vector<int> right_ans;
+    for (int j = 1; j <= m; j++)
+        if (right[j])
+            right_ans.push_back(j);
+
+    cout << left_ans.sz << ' ';
+    for (int x : left_ans)
+        cout << x << ' ';
+    cout << '\n';
+
+    cout << right_ans.sz << ' ';
+    for (int x : right_ans)
+        cout << x << ' ';
+    cout << '\n';
 }
