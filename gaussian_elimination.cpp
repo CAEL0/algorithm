3eea0fa6ddad870d959c7f3c440e9aef7f94aa9e
Jongmin Kim <kjongmin26@gmail.com>
2026-04-13 16:25:51 +0900

    Update gaussian_elimination.cpp

--- gaussian_elimination.cpp
+++ gaussian_elimination.cpp
@@ -18,39 +18,39 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<ll>> v(n, vector<ll>(n + 1));
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j <= n; j++)
+    vector<vector<ll>> v(n + 1, vector<ll>(n + 2));
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n + 1; j++)
             cin >> v[i][j];
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int ii = i + 1; ii < n; ii++) {
+    for (int i = 1; i <= n; i++) {
+        for (int ii = i + 1; ii <= n; ii++) {
             ll k = v[ii][i];
-            for (int j = i; j <= n; j++)
+            for (int j = i; j <= n + 1; j++)
                 v[ii][j] *= v[i][i];
 
-            for (int j = i; j <= n; j++)
+            for (int j = i; j <= n + 1; j++)
                 v[ii][j] -= k * v[i][j];
 
             ll g = v[ii][i];
-            for (int j = i + 1; j <= n; j++)
+            for (int j = i + 1; j <= n + 1; j++)
                 g = gcd(g, v[ii][j]);
 
-            for (int j = i; j <= n; j++)
+            for (int j = i; j <= n + 1; j++)
                 v[ii][j] /= g;
         }
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n; j >= i; j--)
+    for (int i = n; i >= 1; i--) {
+        for (int j = n + 1; j >= i; j--)
             v[i][j] /= v[i][i];
 
-        for (int ii = i - 1; ii >= 0; ii--) {
-            v[ii][n] -= v[ii][i] * v[i][n];
+        for (int ii = i - 1; ii >= 1; ii--) {
+            v[ii][n + 1] -= v[ii][i] * v[i][n + 1];
             v[ii][i] = 0;
         }
     }
 
-    for (int i = 0; i < n; i++)
-        cout << v[i][n] << ' ';
+    for (int i = 1; i <= n; i++)
+        cout << v[i][n + 1] << ' ';
 }
